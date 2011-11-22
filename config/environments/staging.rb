5e1aff61facf3f90c5464727ff09f74fbd798cfe
James Stewart <jys@ketlai.co.uk>
2011-11-22 16:23:38 +0000

    Remove redundant staging environment

--- config/environments/staging.rb
+++
@@ -1,5 +0,0 @@
-require File.expand_path('production.rb', File.dirname(__FILE__))
-Calendars::Application.configure do
-  config.middleware.delete Slimmer::App
-  config.middleware.use Slimmer::App, :asset_host => "http://static.staging.alphagov.co.uk"
-end
