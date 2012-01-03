63df511a0af6a186583ef3e3fd575cb62318b0ef
James Stewart <jys@ketlai.co.uk>
2012-01-03 11:41:10 +0000

    Standardise exeption notification config

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,6 @@
 unless Rails.env.development? or Rails.env.test?
-  Calendars::Application.config.middleware.use ExceptionNotifier,
-    :email_prefix => "[Calendars] ",
+  Rails.application.config.middleware.use ExceptionNotifier,
+    :email_prefix => "[#{Rails.application.split('::').first}] ",
     :sender_address => %{"Winston Smith-Churchill" <winston@alphagov.co.uk>},
     :exception_recipients => %w{govuk-dev@digital.cabinet-office.gov.uk}
 end
