708d2212f76cf2e0fbafce28b42cbc47fb9dbc50
Gareth Rushgrove <gareth@morethanseven.net>
2012-01-20 09:16:30 +0000

    enable template caching in production for slimmer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Calendars::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_controller.asset_host = Plek.current.find('cdn')
+
+  config.slimmer.cache_templates = true
   config.slimmer.asset_host = Plek.current.find('assets')
 
   config.action_mailer.default_url_options = { :host => Plek.current.find('calendars') }
