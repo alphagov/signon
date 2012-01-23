5937da6fa2fd685c246b7b78864272a46049afa5
James Stewart <jys@ketlai.co.uk>
2012-01-23 21:37:02 +0000

    Add another integration test to ensure bad divisions 404

--- test/integration/calendars_test.rb
+++ test/integration/calendars_test.rb
@@ -1,10 +1,14 @@
 require 'integration_test_helper'
 
 class CalendarsTest < ActionDispatch::IntegrationTest
-  
+
   should "give a 404 status when a calendar does not exist" do
     visit '/maternity'
     assert page.status_code == 404
   end
 
+  should "give a 404 when asked for a division that doesn't exist" do
+    visit "/when-do-the-clocks-change/A188770693.ics"
+    assert page.status_code == 404
+  end
 end
