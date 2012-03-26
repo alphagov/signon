556e6548b70589e333b2e01a445c4b111ee2bc3a
James Stewart <james@jystewart.net>
2012-03-26 08:45:42 +0100

    Automate display of next clock change

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,2 +1,13 @@
 module CalendarHelper
+  def next_clock_change
+    @next_clock_change ||= @divisions['united-kingdom'][:whole_calendar].upcoming_event
+  end
+
+  def next_clock_change_description
+    next_clock_change.notes.gsub(' one hour', '').downcase
+  end
+
+  def next_clock_change_date
+    @divisions['united-kingdom'][:whole_calendar].upcoming_event.date.strftime('%d %B')
+  end
 end
