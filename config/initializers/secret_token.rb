d82798abe10189930f89803e959fb3494b956f9b
Blaine Cook <romeda@gmail.com>
2012-04-02 11:51:39 +0100

    initial import, default rails 3.2.3 build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Signonotron2::Application.config.secret_token = 'f99800e81efe05fea66d74a99ec814800801f3cc2e03ace42415758a85dea2468b5e2ba570b54f1d9e9295fa9a3da4e64585aa233a997dd592ac0960a4528e84'
