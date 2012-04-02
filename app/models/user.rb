deee8c071b5c72cc98c8bc5b3dd25dbdda21745d
Blaine Cook <romeda@gmail.com>
2012-04-02 13:16:22 +0100

    update devise options on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :trackable,
+         :validatable, :timeoutable, :lockable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :uid, :name, :email, :password, :password_confirmation, :twitter, :github, :beard
+  attr_readonly :uid
+
 end
