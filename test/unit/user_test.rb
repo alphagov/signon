13de02e31ae4f8fabd3a6648e94ca8589d03530a
Blaine Cook <romeda@gmail.com>
2012-04-04 13:52:30 +0100

    improve unit test coverage for password strength

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -25,4 +25,20 @@ class UserTest < ActiveSupport::TestCase
   test "secure gravatar urls should be generatable" do
     assert_equal "https://secure.gravatar.com/avatar/#{Digest::MD5.hexdigest(@user.email)}", @user.gravatar_url(:ssl => true)
   end
+
+  # Password Validation
+
+  test "it requires a password to be at least 10 characters long" do
+    u = FactoryGirl.build(:user, :password => "dNG.c0w5!")
+    assert !u.valid?
+    assert_not_empty u.errors[:password]
+  end
+
+  test "it allows very long passwords with spaces" do
+    u = FactoryGirl.build(:user, :password => ("4 l0nG sT!,ng " * 10)[0..127])
+    u.valid?
+    assert u.valid?
+    assert_empty u.errors[:password]
+  end
+
 end
