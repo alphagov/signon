af92481ddcf7be47be3ed169d635eea83288eb1e
Blaine Cook <romeda@gmail.com>
2012-04-04 13:45:55 +0100

    update user test to be a bit more sensible about object creation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = FactoryGirl.create(:user)
+  end
+
+  # JSON Output
+
   test "sensible json output" do
-    user = FactoryGirl.create(:user)
-    assert_equal( { 'email' =>  user.email }.to_json, user.to_sensible_json )
+    assert_equal( { 'email' =>  @user.email }.to_json, @user.to_sensible_json )
   end
 end
