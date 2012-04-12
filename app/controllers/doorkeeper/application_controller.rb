694956e0eadaeeaa5446076b0ee0c7c9666180e7
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-04-12 11:36:09 +0100

    Skip slimmer for doorkeeper admin pages.

---
+++ app/controllers/doorkeeper/application_controller.rb
@@ -0,0 +1,10 @@
+# Need to load because in development mode this file gets re-evaluated on every request and a require
+# would only load the parent app controller on the first request.
+load Doorkeeper::Engine.config.root.join 'app', 'controllers', 'doorkeeper', 'application_controller.rb'
+
+Doorkeeper::ApplicationController.class_eval do
+  before_filter do
+    headers["X-Slimmer-Skip"] = "1"
+    Rails.logger.info "Set X-Slimmer-Skip=1"
+  end
+end
