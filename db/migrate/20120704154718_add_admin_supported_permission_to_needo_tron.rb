2afd26009e5c734726426d7b74279bdb030614eb
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-07-04 16:50:53 +0100

    Add 'admin' permission for Need-o-Tron

---
+++ db/migrate/20120704154718_add_admin_supported_permission_to_needo_tron.rb
@@ -0,0 +1,12 @@
+class AddAdminSupportedPermissionToNeedoTron < ActiveRecord::Migration
+  class SupportedPermission < ActiveRecord::Base
+    belongs_to :application, class_name: 'Doorkeeper::Application'
+  end
+
+  def change
+    needotron = ::Doorkeeper::Application.find_by_name("Need-o-Tron")
+    if needotron
+      SupportedPermission.create!(application: needotron, name: "admin")
+    end
+  end
+end
