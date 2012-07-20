3d81f8f3c7a6989c5711b7480c92803020e898c7
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-07-20 16:15:11 +0100

    Existing permissions were synced when they were updated
    Trying to avoid creating a negative impression about the reliability of
    the "Last synced at" column.

--- db/migrate/20120720131717_add_last_synced_at_to_permission.rb
+++ db/migrate/20120720131717_add_last_synced_at_to_permission.rb
@@ -1,5 +1,6 @@
 class AddLastSyncedAtToPermission < ActiveRecord::Migration
-  def change
+  def up
     add_column :permissions, :last_synced_at, :datetime
+    execute("update permissions set last_synced_at = updated_at")
   end
 end
