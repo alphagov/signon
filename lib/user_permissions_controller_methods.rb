98a119665aedb16efa6613b0625a95c063ddcc2d
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-08-30 10:25:51 +0000

    Fix intermittent failure when editing permissions
    From close inspection of the parameters from when the errors occurred,
    an ID was not being supplied by the browser for the permission which
    the server was trying to create.
    
    I believe that the following happened (and I successfully reproduced):
     * The user was created/edited once, and they did not have a pre-existing permission
       for one or more application. The server therefore created it on submit.
     * The admin user clicked back (i.e. not being rendered a fresh page containing the
       new permission ID)
     * They submitted again, and the server tried to create another permission, but failed
       because it already existed.
    
    The fix, therefore, is to always create the permission before it is
    rendered to the browser, hence it always has an ID.

--- lib/user_permissions_controller_methods.rb
+++ lib/user_permissions_controller_methods.rb
@@ -2,8 +2,7 @@ module UserPermissionsControllerMethods
   private
     def applications_and_permissions(user)
       ::Doorkeeper::Application.order(:name).all.map do |application|
-        permission_for_application = user.permissions.find_by_application_id(application.id)
-        permission_for_application ||= Permission.new(application: application, user: user)
+        permission_for_application = user.permissions.find_or_create_by_application_id(application.id)
         [application, permission_for_application]
       end
     end
