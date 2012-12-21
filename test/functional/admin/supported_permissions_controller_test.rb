be3c98417fa2e12497c91020fcbb6cfaf8045a08
David Illsley <david.illsley@digital.cabinet-office.gov.uk>
2012-12-21 10:01:51 +0000

    Add functional tests

---
+++ test/functional/admin/supported_permissions_controller_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class Admin::SupportedPermissionsControllerTest < ActionController::TestCase
+
+  setup do
+    @user = FactoryGirl.create(:user, is_admin: true)
+    sign_in @user
+  end
+
+  context "GET index" do
+    should "render the form" do
+      app = FactoryGirl.create(:application, name: "My first app")
+      perm = FactoryGirl.create(:supported_permission, application_id: app.id, name: "permission1")
+      get :index, application_id: app.id
+      assert_select "h1", /My first app/
+      assert_select "p[id='permissions']", /permission1/
+      assert_select "a[id='add']", true
+      assert_select "a[id='cancel']", true
+    end
+  end
+
+  context "GET new" do
+    should "render the form" do
+      app = FactoryGirl.create(:application, name: "My first app")
+      perm = FactoryGirl.create(:supported_permission, application_id: app.id, name: "permission1")
+      get :new, application_id: app.id
+      assert_select "h1", /My first app/
+      assert_select "input[name='post[permission]']", true
+    end
+  end
+
+  context "GET create" do
+    should "create a new permission" do
+      app = FactoryGirl.create(:application, name: "My first app")
+      perm = FactoryGirl.create(:supported_permission, application_id: app.id, name: "permission1")
+      post :create, application_id: app.id, post:{ permission: "permission2" }
+      assert_redirected_to(:controller => "supported_permissions", :action => :index)
+      get :index, application_id: app.id
+      assert_select "h1", /My first app/
+      assert_select "p[id='permissions']", /permission1, permission2/
+     end
+  end
+
+end
