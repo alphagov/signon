ab69d1acf091c257df17b92b66a8490fcd7387a0
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-01-15 17:59:48 +0000

    Update link text to user edit page

--- features/support/passphrase_support.rb
+++ features/support/passphrase_support.rb
@@ -1,7 +1,7 @@
 module PassPhraseSupport
   def change_password(from, to, confirmation)
     visit root_path
-    click_link "Change your passphrase"
+    click_link "Change your email or passphrase"
     fill_in "Current passphrase", with: from
     fill_in "New passphrase", with: to
     fill_in "Confirm new passphrase", with: confirmation
