0f49b28d2f43002eeca8664500f01493f8868f36
James Stewart <jys@ketlai.co.uk>
2013-02-10 11:56:30 +0000

    Uncomment param parser removal
    Thanks @jamiecobbett - I had commented them for testing and forgot to reinstate

--- config/initializers/restrict_parsers.rb
+++ config/initializers/restrict_parsers.rb
@@ -1,4 +1,4 @@
 # Turn off XML parsing:
 # https://groups.google.com/forum/#!topic/rubyonrails-security/61bkgvnSGTQ/discussion
-# ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
-# ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::JSON)
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::JSON)
