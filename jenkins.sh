f86d04a119dbe250bd817d43962d9b44e1f275a2
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-02-25 10:46:57 +0000

    Remove redundant govuk_setenv
    This broke the build on CI because it doesn't have the command.

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-govuk_setenv signon bundle exec rake stats
-govuk_setenv signon bundle exec rake db:drop db:create db:schema:load
-govuk_setenv signon env USE_SIMPLECOV=true RAILS_ENV=test bundle exec rake --trace 
+bundle exec rake stats
+bundle exec rake db:drop db:create db:schema:load
+USE_SIMPLECOV=true RAILS_ENV=test bundle exec rake --trace 
 RESULT=$?
 exit $RESULT
