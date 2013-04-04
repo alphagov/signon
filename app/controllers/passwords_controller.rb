a044b79f77072f3feb79ce39f782c5ab341e24f8
James Stewart <jys@ketlai.co.uk>
2013-04-04 09:37:36 +0100

    Change password reset token when reset link is followed
    This means that a password reset token will be changed within seconds
    of being logged. That does mean that the links in emails will only
    work once, so copy changes should follow to explain that.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,6 +1,21 @@
 class PasswordsController < Devise::PasswordsController
   before_filter :record_password_reset_request, only: :create
 
+  def edit
+    self.resource = resource_class.where(reset_password_token: params[:reset_password_token]).first
+    raise ActiveRecord::RecordNotFound unless self.resource
+
+    # TODO: Test the error condition here
+    if self.resource.reset_password_period_valid?
+      # Unfortunately (and surprisingly) the model method to change the reset password
+      # token is protected. It seems better to break that protection than to reimplement
+      # the same feature ourselves.
+      self.resource.__send__(:generate_reset_password_token!)
+    else
+      render_406
+    end
+  end
+
   # Copied from https://github.com/plataformatec/devise/blob/v2.1.2/app/controllers/devise/passwords_controller.rb#L12
   # So that we can rescue from SES blacklist errors and behave normally 
   # (i.e. not giving away whether or not an account exists for the email)
