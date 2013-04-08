dc35d08f5b1bc7a5a7b7280751b76548a0f7f46a
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-04-08 16:55:33 +0000

    Add basic implementation of batch user create

--- app/models/batch_invitation.rb
+++ app/models/batch_invitation.rb
@@ -11,8 +11,25 @@ class BatchInvitation < ActiveRecord::Base
     outcome.nil?
   end
 
+  def perform(options = {})
+    self.batch_invitation_users.each do |bi_user|
+      attributes = {
+        name: bi_user.name,
+        email: bi_user.email,
+        permissions_attributes: self.applications_and_permissions
+      }
+      begin
+        User.invite!(attributes, self.user)
+      rescue StandardError => e
+      end
+    end
+    self.outcome = "success"
+    self.save!
+  end
+
   class Job < Struct.new(:id)
     def perform(options = {})
+      BatchInvitation.find(id).perform(options)
     end
   end
 end
