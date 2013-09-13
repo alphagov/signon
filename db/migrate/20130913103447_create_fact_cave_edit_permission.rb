25101ae4cd73f7c05293fa0a6fd1e75c496820ee
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-09-13 16:00:10 +0000

    Initialise then save edit_fact permission
    This circumvents protected attributes which prevented the application
    association hookup.

--- db/migrate/20130913103447_create_fact_cave_edit_permission.rb
+++ db/migrate/20130913103447_create_fact_cave_edit_permission.rb
@@ -1,12 +1,10 @@
 class CreateFactCaveEditPermission < ActiveRecord::Migration
-  class SupportedPermission < ActiveRecord::Base
-    belongs_to :application, class_name: 'Doorkeeper::Application'
-  end
 
   def up
     fact_cave = ::Doorkeeper::Application.find_by_name("Fact Cave")
     if fact_cave
-      SupportedPermission.create!(application: fact_cave, name: "edit_fact")
+      permission = SupportedPermission.new(application_id: fact_cave.to_param, name: "edit_fact")
+      permission.save!
     end
  
   end
