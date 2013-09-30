83b8ec5945a89e2ed193213d9c9ab72359c2dee0
Jake Benilov <benilov@gmail.com>
2013-09-30 11:01:15 +0000

    guard for dev environments where support app absent

--- db/migrate/20130926134720_remove_support_app_ertp_permission.rb
+++ db/migrate/20130926134720_remove_support_app_ertp_permission.rb
@@ -7,12 +7,14 @@ class RemoveSupportAppErtpPermission < ActiveRecord::Migration
   def up
     support = ::Doorkeeper::Application.find_by_name("Support")
 
-    permission = SupportedPermission.find_by_application_id_and_name(support.id, "ertp")
-    permission.delete unless permission.nil?
+    unless support.nil?
+      permission = SupportedPermission.find_by_application_id_and_name(support.id, "ertp")
+      permission.delete unless permission.nil?
 
-    # remove user permissions
-    all_support_perms = Permission.where(application_id: support.id)
-    ertp_support_perms = all_support_perms.select { |perm| perm.permissions.include?("ertp") }
-    ertp_support_perms.each { |perm| perm.permissions -= ["ertp"]; perm.save! }
+      # remove user permissions
+      all_support_perms = Permission.where(application_id: support.id)
+      ertp_support_perms = all_support_perms.select { |perm| perm.permissions.include?("ertp") }
+      ertp_support_perms.each { |perm| perm.permissions -= ["ertp"]; perm.save! }
+    end
   end
 end
