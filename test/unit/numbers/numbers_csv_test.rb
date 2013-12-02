9e8165e2c41502e7e59a02d589192d3bc28f046a
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2013-12-02 11:20:43 +0000

    More readable tests, conforming to *nix standards

--- test/unit/numbers/numbers_csv_test.rb
+++ test/unit/numbers/numbers_csv_test.rb
@@ -5,22 +5,26 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
   def setup
     FactoryGirl.create(:admin_user, email: 'admin_user@admin.example.com')
-    3.times {|i| FactoryGirl.create(:user) }
+    FactoryGirl.create_list(:user, 3)
   end
 
   def teardown
     `rm ./numbers.csv`
   end
 
+  def numbers_csv
+    CSV.parse(File.read(Rails.root + 'numbers.csv'))
+  end
+
   test "csv contains accounts count" do
     NumbersCsv.generate
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count", "total", "4"]
+    assert numbers_csv.include? ["Accounts count", "total", "4"]
   end
 
   test "csv contains counts by role" do
     NumbersCsv.generate
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by role", "admin", "1"]
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by role", "normal", "3"]
+    assert numbers_csv.include? ["Accounts count by role", "admin", "1"]
+    assert numbers_csv.include? ["Accounts count by role", "normal", "3"]
   end
 
   test "csv contains counts by account state" do
@@ -28,8 +32,8 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
     NumbersCsv.generate
 
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by state", "active", "3"]
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by state", "suspended", "1"]
+    assert numbers_csv.include? ["Accounts count by state", "active", "3"]
+    assert numbers_csv.include? ["Accounts count by state", "suspended", "1"]
   end
 
   test "csv contains counts by application access" do
@@ -39,7 +43,7 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
     NumbersCsv.generate
 
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by application", "WhiteCloud", "1"]
+    assert numbers_csv.include? ["Accounts count by application", "WhiteCloud", "1"]
   end
 
   test "csv contains counts by organisation" do
@@ -48,8 +52,8 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
     NumbersCsv.generate
 
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by organisation", "Ministry of Digital", "1"]
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by organisation", "None assigned", "3"]
+    assert numbers_csv.include? ["Accounts count by organisation", "Ministry of Digital", "1"]
+    assert numbers_csv.include? ["Accounts count by organisation", "None assigned", "3"]
   end
 
   test "csv contains counts by days of inactivity" do
@@ -57,17 +61,17 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
     NumbersCsv.generate
 
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by days inactive", "7+", "2"]
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by days inactive", "15+", "1"]
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by days inactive", "30+", "0"]
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by days inactive", "60+", "0"]
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by days inactive", "90+", "0"]
+    assert numbers_csv.include? ["Accounts count by days inactive", "7+", "2"]
+    assert numbers_csv.include? ["Accounts count by days inactive", "15+", "1"]
+    assert numbers_csv.include? ["Accounts count by days inactive", "30+", "0"]
+    assert numbers_csv.include? ["Accounts count by days inactive", "60+", "0"]
+    assert numbers_csv.include? ["Accounts count by days inactive", "90+", "0"]
   end
 
   test "csv contains counts by email domain" do
     NumbersCsv.generate
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by email domain", "admin.example.com", "1"]
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by email domain", "example.com", "3"]
+    assert numbers_csv.include? ["Accounts count by email domain", "admin.example.com", "1"]
+    assert numbers_csv.include? ["Accounts count by email domain", "example.com", "3"]
   end
 
   test "csv contains counts by application access per organisation" do
@@ -80,7 +84,7 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
     NumbersCsv.generate
 
-    assert CSV.parse(File.read(Rails.root + 'numbers.csv')).include? ["Accounts count by application per organisation", "Ministry of Digital", "WhiteCloud", "1"]
+    assert numbers_csv.include? ["Accounts count by application per organisation", "Ministry of Digital", "WhiteCloud", "1"]
   end
 
 end
