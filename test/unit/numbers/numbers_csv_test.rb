728af31840f733718c6f3af9dd9397af04f05545
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2013-12-02 13:49:36 +0000

    Ignore suspended accounts to get real picture

--- test/unit/numbers/numbers_csv_test.rb
+++ test/unit/numbers/numbers_csv_test.rb
@@ -21,12 +21,6 @@ class NumbersCsvTest < ActiveSupport::TestCase
     assert numbers_csv.include? ["Accounts count", "total", "4"]
   end
 
-  test "csv contains counts by role" do
-    NumbersCsv.generate
-    assert numbers_csv.include? ["Accounts count by role", "admin", "1"]
-    assert numbers_csv.include? ["Accounts count by role", "normal", "3"]
-  end
-
   test "csv contains counts by account state" do
     User.last.suspend('test')
 
@@ -36,6 +30,12 @@ class NumbersCsvTest < ActiveSupport::TestCase
     assert numbers_csv.include? ["Accounts count by state", "suspended", "1"]
   end
 
+  test "csv contains counts by role" do
+    NumbersCsv.generate
+    assert numbers_csv.include? ["Active accounts count by role", "admin", "1"]
+    assert numbers_csv.include? ["Active accounts count by role", "normal", "3"]
+  end
+
   test "csv contains counts by application access" do
     app = FactoryGirl.create(:application, name: "WhiteCloud")
     FactoryGirl.create(:supported_permission, name: "signin", application: app)
@@ -43,7 +43,7 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
     NumbersCsv.generate
 
-    assert numbers_csv.include? ["Accounts count by application", "WhiteCloud", "1"]
+    assert numbers_csv.include? ["Active accounts count by application", "WhiteCloud", "1"]
   end
 
   test "csv contains counts by organisation" do
@@ -52,8 +52,8 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
     NumbersCsv.generate
 
-    assert numbers_csv.include? ["Accounts count by organisation", "Ministry of Digital", "1"]
-    assert numbers_csv.include? ["Accounts count by organisation", "None assigned", "3"]
+    assert numbers_csv.include? ["Active accounts count by organisation", "Ministry of Digital", "1"]
+    assert numbers_csv.include? ["Active accounts count by organisation", "None assigned", "3"]
   end
 
   test "csv contains counts by days of inactivity" do
@@ -61,17 +61,17 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
     NumbersCsv.generate
 
-    assert numbers_csv.include? ["Accounts count by days inactive", "7+", "2"]
-    assert numbers_csv.include? ["Accounts count by days inactive", "15+", "1"]
-    assert numbers_csv.include? ["Accounts count by days inactive", "30+", "0"]
-    assert numbers_csv.include? ["Accounts count by days inactive", "60+", "0"]
-    assert numbers_csv.include? ["Accounts count by days inactive", "90+", "0"]
+    assert numbers_csv.include? ["Active accounts count by days inactive", "7+", "2"]
+    assert numbers_csv.include? ["Active accounts count by days inactive", "15+", "1"]
+    assert numbers_csv.include? ["Active accounts count by days inactive", "30+", "0"]
+    assert numbers_csv.include? ["Active accounts count by days inactive", "60+", "0"]
+    assert numbers_csv.include? ["Active accounts count by days inactive", "90+", "0"]
   end
 
   test "csv contains counts by email domain" do
     NumbersCsv.generate
-    assert numbers_csv.include? ["Accounts count by email domain", "admin.example.com", "1"]
-    assert numbers_csv.include? ["Accounts count by email domain", "example.com", "3"]
+    assert numbers_csv.include? ["Active accounts count by email domain", "admin.example.com", "1"]
+    assert numbers_csv.include? ["Active accounts count by email domain", "example.com", "3"]
   end
 
   test "csv contains counts by application access per organisation" do
@@ -84,7 +84,7 @@ class NumbersCsvTest < ActiveSupport::TestCase
 
     NumbersCsv.generate
 
-    assert numbers_csv.include? ["Accounts count by application per organisation", "Ministry of Digital", "WhiteCloud", "1"]
+    assert numbers_csv.include? ["Active accounts count by application per organisation", "Ministry of Digital", "WhiteCloud", "1"]
   end
 
 end
