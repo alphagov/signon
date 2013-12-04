d5d609e8735f64ab0b5afe74ae820d634fb075d0
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2013-12-04 11:53:58 +0000

    Correcting test and domain mappings

--- test/unit/organisation_mappings/zendesk_to_signon_test.rb
+++ test/unit/organisation_mappings/zendesk_to_signon_test.rb
@@ -4,16 +4,16 @@ require Rails.root + 'lib/organisation_mappings/zendesk_to_signon'
 class OrganisationMappings::ZendeskToSignonTest < ActiveSupport::TestCase
 
   test "assigns organisation to users who have recognised domain names" do
-    org = FactoryGirl.create(:organisation, name: "Cabinet Office")
+    co = FactoryGirl.create(:organisation, name: "Cabinet Office")
     user = FactoryGirl.create(:user, email: 'foo@digital.cabinet-office.gov.uk')
 
-    assert_empty org.users
+    assert_empty co.users
     OrganisationMappings::ZendeskToSignon.apply
-    assert_equal org, user.reload.organisation
+    assert_equal co, user.reload.organisation
   end
 
   test "doesn't assign organisation to users who don't have a recognised domain name" do
-    org = FactoryGirl.create(:organisation, name: "Cabinet Office")
+    co = FactoryGirl.create(:organisation, name: "Cabinet Office")
     user = FactoryGirl.create(:user, email: 'foo@mailinator.com')
 
     OrganisationMappings::ZendeskToSignon.apply
@@ -21,11 +21,12 @@ class OrganisationMappings::ZendeskToSignonTest < ActiveSupport::TestCase
   end
 
   test "doesn't affect users who belong to an organisation" do
-    org = FactoryGirl.create(:organisation, name: "Cabinet Office")
-    org.users << (user = FactoryGirl.create(:user, email: 'foo@mailinator.com'))
+    hmrc = FactoryGirl.create(:organisation, name: "HM Revenue & Customs")
+    co = FactoryGirl.create(:organisation, name: "Cabinet Office")
+    co.users << (co_user = FactoryGirl.create(:user, email: 'someone.important@hmrc.gsi.gov.uk'))
 
     OrganisationMappings::ZendeskToSignon.apply
-    assert_equal org, user.organisation
+    assert_equal co, co_user.reload.organisation
   end
 
 end
