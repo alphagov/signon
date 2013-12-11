d1eece6c8ff406c5fce8aed40c3a76f4c7bb088c
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2013-12-11 17:32:12 +0000

    Validate users before inviting

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,10 +1,10 @@
 class Permission < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, inverse_of: :permissions
   belongs_to :application, class_name: 'Doorkeeper::Application'
   serialize :permissions, Array
 
-  validates_presence_of :application_id
-  validates_presence_of :user_id
+  validates_presence_of :application
+  validates_presence_of :user
 
   def synced!
     update_attributes(last_synced_at: Time.zone.now)
--- lib/user_permissions_controller_methods.rb
+++ lib/user_permissions_controller_methods.rb
@@ -2,7 +2,7 @@ module UserPermissionsControllerMethods
   private
     def applications_and_permissions(user)
       ::Doorkeeper::Application.order(:name).all.map do |application|
-        permission_for_application = user.permissions.find_or_create_by_application_id(application.id)
+        permission_for_application = Permission.where(application_id: application.id, user_id: user.id).first_or_initialize
         [application, permission_for_application]
       end
     end
