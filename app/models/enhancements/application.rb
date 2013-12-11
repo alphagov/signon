991e2bd53d35feae898f0c083292cf6aca1aba68
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2013-12-11 15:05:39 +0000

    Using Cancan helpers to simplify controller

--- app/models/enhancements/application.rb
+++ app/models/enhancements/application.rb
@@ -6,6 +6,8 @@ class ::Doorkeeper::Application < ActiveRecord::Base
 
   attr_accessible :name, :description, :uid, :secret, :redirect_uri, :home_uri
 
+  default_scope order(:name)
+
   def self.default_permission_strings
     # Excludes user_update_permission which is granted automatically when needed
     # to the special SSO Push User
