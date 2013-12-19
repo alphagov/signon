05b1411fbb0b9bad90ea075374f753b590f50adf
Jake Benilov <benilov@gmail.com>
2013-12-19 13:03:26 +0000

    api auth test: use rack-test instead of capybara

--- test/helpers/token_auth_support.rb
+++ test/helpers/token_auth_support.rb
@@ -1,6 +1,6 @@
 module TokenAuthSupport
   def get_valid_token
-    token = User.last.authorisations.first
+    User.last.authorisations.first
   end
 
   def get_expired_token
@@ -11,14 +11,6 @@ module TokenAuthSupport
   end
 
   def get_revoked_token
-    token = get_valid_token
-    token.revoke
-    token
-  end
-
-  def set_bearer_token(token)
-    options = page.driver.instance_variable_get("@options")
-    options[:headers] = { "HTTP_AUTHORIZATION" => "Bearer #{token}" }
-    page.driver.instance_variable_set "@options", options
+    get_valid_token.tap { |token| token.revoke }
   end
 end
--- test/integration/api_authentication_test.rb
+++ test/integration/api_authentication_test.rb
@@ -4,6 +4,10 @@ require 'helpers/token_auth_support'
 class ApiAuthenticationTest < ActionDispatch::IntegrationTest
   include TokenAuthSupport
 
+  def access_user_endpoint(token = nil)
+    get "/user.json", {}, token.nil? ? {} : {"HTTP_AUTHORIZATION" => "Bearer #{token}"}
+  end
+
   setup do
     app = create(:application, name: "MyApp", with_supported_permissions: ["write"])
     user = create(:user, with_permissions: { app => ["write"] })
@@ -11,38 +15,34 @@ class ApiAuthenticationTest < ActionDispatch::IntegrationTest
   end
 
   should "grant access to the user details with a valid access token" do
-    set_bearer_token(get_valid_token.token)
-    visit "/user.json"
+    access_user_endpoint(get_valid_token.token)
 
-    parsed_response = JSON.parse(page.source)
+    parsed_response = JSON.parse(response.body)
     assert parsed_response.has_key?('user')
     assert parsed_response['user']['permissions'].is_a?(Array)
   end
 
   should "not grant access without an access token" do
-    visit "/user.json"
+    access_user_endpoint
 
-    assert_equal 401, page.status_code
+    assert_equal 401, response.status
   end
 
   should "not grant access with an invalid access token" do
-    set_bearer_token(get_valid_token.token.reverse)
-    visit "/user.json"
+    access_user_endpoint(get_valid_token.token.reverse)
 
-    assert_equal 401, page.status_code
+    assert_equal 401, response.status
   end
 
   should "not grant access when access token has expired" do
-    set_bearer_token(get_expired_token.token)
-    visit "/user.json"
+    access_user_endpoint(get_expired_token.token)
 
-    assert_equal 401, page.status_code
+    assert_equal 401, response.status
   end
 
   should "not grant access when access token has been revoked" do
-    set_bearer_token(get_revoked_token.token)
-    visit "/user.json"
+    access_user_endpoint(get_revoked_token.token)
 
-    assert_equal 401, page.status_code
+    assert_equal 401, response.status
   end
 end
