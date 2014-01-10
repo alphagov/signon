e6fa95cec8db51cb59b4fd7b39bfed7b41b65974
Jake Benilov <benilov@gmail.com>
2014-01-10 18:16:36 +0000

    improve docs and error checking for UserAccountOperations test code

--- test/helpers/user_account_operations.rb
+++ test/helpers/user_account_operations.rb
@@ -1,5 +1,9 @@
 module UserAccountOperations
+  # usage: accept_invitation(password: "<new password>", invitation_token: "<token>")
   def accept_invitation(options)
+    raise "Please provide password" unless options[:password]
+    raise "Please provide invitation token" unless options[:invitation_token]
+
     signout
     visit accept_user_invitation_path(invitation_token: options[:invitation_token])
     fill_in "Passphrase", with: options[:password]
@@ -7,7 +11,11 @@ module UserAccountOperations
     click_button "Set my passphrase"
   end
 
+  # usage: confirm_email_change(password: "<new password>", confirmation_token: "<token>")
   def confirm_email_change(options)
+    raise "Please provide password" unless options[:password]
+    raise "Please provide confirmation token" unless options[:confirmation_token]
+
     signout
     visit user_confirmation_path(confirmation_token: options[:confirmation_token])
     assert_response_contains("Confirm a change to your account email")
