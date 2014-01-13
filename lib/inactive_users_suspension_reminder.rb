487a66b5c3521b429ca0833362b798a6a7831373
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-01-13 14:58:47 +0000

    Making suspension reminders delivery resilient
    also had to move to contexts in tests to have
    setups per context block.

--- lib/inactive_users_suspension_reminder.rb
+++ lib/inactive_users_suspension_reminder.rb
@@ -1,10 +1,14 @@
 class InactiveUsersSuspensionReminder
-
   def send_reminders
     mailing_list = users_by_days_to_suspension
     mailing_list.each do |days, users|
       users.each do |user|
-        UserMailer.suspension_reminder(user, days).deliver
+        tries = 3
+        begin
+          UserMailer.suspension_reminder(user, days).deliver
+        rescue *network_errors => e
+          retry if (tries -= 1) > 0
+        end
       end
     end
     mailing_list.values.flatten.uniq.count
@@ -17,6 +21,12 @@ class InactiveUsersSuspensionReminder
     end
     users_by_days_to_suspension[1] += User.last_signed_in_before(SUSPENSION_THRESHOLD_PERIOD.next.days.ago).to_a
     users_by_days_to_suspension
+  end
+
+private
+
+  def network_errors
+    [SocketError, Timeout::Error, Errno::ECONNREFUSED, Errno::EHOSTDOWN, Errno::EHOSTUNREACH, Errno::ETIMEDOUT]
   end
 
 end
