1b3dee7189264a58c97ff758b622a90ee58c7aa3
Jake Benilov <benilov@gmail.com>
2014-01-14 11:26:59 +0000

    port dashboard test to rails integration

---
+++ test/integration/dashboard_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+ 
+class DashboardTest < ActionDispatch::IntegrationTest
+  should "notify the user if they've not been assigned any applications" do
+    user = create(:user)
+    visit root_path
+    signin(user)
+
+    assert_response_contains("Your Applications")
+    assert_response_contains("You aren't yet assigned to any applications")
+  end
+
+  should "show the user's assigned applications" do
+    app = create(:application, name: "MyApp", with_supported_permissions: ["signin"])
+    user = create(:user, with_signin_permissions_for: [ app ] )
+
+    visit root_path
+    signin(user)
+
+    assert_response_contains(app.description)
+    assert page.has_css?("a[href='#{app.home_uri}']")
+  end
+end
