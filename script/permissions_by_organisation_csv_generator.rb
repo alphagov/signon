a7d3dc0eb1d18df864bbaa8aa3492b40b623ac7d
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-01-15 12:41:46 +0000

    Permissions by organisation csv generator

---
+++ script/permissions_by_organisation_csv_generator.rb
@@ -0,0 +1,28 @@
+require 'csv'
+
+class PermissionsByOrganisationCsvGenerator
+  def self.generate
+    filename = 'permissions_by_organisation.csv'
+    CSV.open(filename, "wb") do |csv|
+      csv << ['Organisation', 'Application', 'Permission']
+      permissions_by_organisation.each { |line| csv << line }
+    end
+    puts "Permissions by organisations saved to ./#{filename}"
+  end
+
+  def self.permissions_by_organisation
+    permissions_by_organisation = []
+    Organisation.includes(users: { permissions: :application }).each do |organisation|
+      organisation.users.each do |user|
+        user.permissions.each do |permission|
+          permission.permissions.each do |permission_name|
+            permissions_by_organisation << [organisation.name, permission.application.name, permission_name]
+          end
+        end
+      end
+    end
+    permissions_by_organisation.uniq
+  end
+end
+
+PermissionsByOrganisationCsvGenerator.generate
