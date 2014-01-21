c77a6b725563bc3e2b4101ef28860f9e3ba29899
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-01-21 16:55:12 +0000

    Make exception_notification work with Sidekiq
    Sidkiq handles exceptions raised in workers and notifies
    via exception_notification, for which it depends on this
    version of exception_notification.
    
    also, correcting the exception_notification config to
    send the correct application name in email subject.

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'alphabetical_paginate', '2.1.0'
 gem 'mysql2'
 gem 'aws-ses', require: 'aws/ses'
 gem 'jquery-rails'
-gem 'exception_notification'
+gem 'exception_notification', '4.0.1'
 gem 'plek', '1.4.0'
 gem 'json', '1.7.7'
 gem 'whenever', '0.7.3', require: false
--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,9 @@
-unless Rails.env.development? or Rails.env.test?
-  Rails.application.config.middleware.use ExceptionNotifier,
-    :email_prefix => "[#{Rails.application.to_s.split('::').first}] ",
+require 'exception_notification/rails'
+
+ExceptionNotification.configure do |config|
+  config.add_notifier :email, {
+    :email_prefix => "[#{Rails.application.class.parent_name}] ",
     :sender_address => %{"Winston Smith-Churchill" <winston@alphagov.co.uk>},
     :exception_recipients => %w{govuk-exceptions@digital.cabinet-office.gov.uk}
+  }
 end
