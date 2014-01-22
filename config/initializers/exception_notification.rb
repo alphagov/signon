6b87563d41c91bd3f9e57bc1681b546a16546d9a
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-01-22 10:58:39 +0000

    Removing this config, as it's replaced on deploy

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,9 +1,2 @@
-require 'exception_notification/rails'
-
-ExceptionNotification.configure do |config|
-  config.add_notifier :email, {
-    :email_prefix => "[#{Rails.application.class.parent_name}] ",
-    :sender_address => %{"Winston Smith-Churchill" <winston@alphagov.co.uk>},
-    :exception_recipients => %w{govuk-exceptions@digital.cabinet-office.gov.uk}
-  }
-end
+# This file is here only for reference.
+# ExceptionNotification is configured from alphagov-deployment.
