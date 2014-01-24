72024e538a8d3878c548952c3487768a41255d61
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-01-24 17:21:25 +0000

    Signon logout triggers logout on downstream apps

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -245,6 +245,9 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
+  Warden::Manager.before_logout do |user, auth, opts|
+    ReauthEnforcer.perform_on(user)
+  end
 
   # ==> Custom Validators for Devise
   require 'devise/models/suspendable'
