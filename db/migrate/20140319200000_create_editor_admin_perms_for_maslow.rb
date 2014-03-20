0f3bda555ba8caa99cc839bb82c3a66a9fd91ed0
Jake Benilov <benilov@gmail.com>
2014-03-20 14:24:44 +0000

    Maslow: create editor, admin permissions

---
+++ db/migrate/20140319200000_create_editor_admin_perms_for_maslow.rb
@@ -0,0 +1,14 @@
+class CreateEditorAdminPermsForMaslow < ActiveRecord::Migration
+  class ::Doorkeeper::Application < ActiveRecord::Base; end
+  class SupportedPermission < ActiveRecord::Base
+    belongs_to :application, class_name: 'Doorkeeper::Application'
+  end
+
+  def up
+    maslow = ::Doorkeeper::Application.where(name: "Maslow").first
+    if maslow
+      SupportedPermission.create!(application: maslow, name: "editor")
+      SupportedPermission.create!(application: maslow, name: "admin")
+    end
+  end
+end
