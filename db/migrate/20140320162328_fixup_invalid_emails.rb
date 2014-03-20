f2f84b16b212d9a0ec79afc7052c876f70189151
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-03-20 16:47:30 +0000

    Migration to fixup invalid email addresses
    We've got some bad data in the database.  We have emails ending with
    various noise, ranging from '>', ',' to odd unicode whitespace
    characters (eg U2000E).

---
+++ db/migrate/20140320162328_fixup_invalid_emails.rb
@@ -0,0 +1,22 @@
+class FixupInvalidEmails < ActiveRecord::Migration
+  class TempUser < ActiveRecord::Base
+    self.table_name = 'users'
+  end
+
+  def up
+    TempUser.find_each do |user|
+      # There are entries in the db with various invalid chars on the end.
+      # These range from commas to odd unicode whitespace chars (eg U200E)
+      # The last char of any valid email address should be an ascii letter.
+      cleaned = user.email.sub(/[^a-z]+\z/i, '')
+      if user.email != cleaned
+        puts "Fixing email address for '#{user.email}'"
+        user.email = cleaned
+        user.save!
+      end
+    end
+  end
+
+  def down
+  end
+end
