17b76c93a0abf06a137b0941c5320e95868e4516
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-06-23 10:35:18 +0000

    EventLog optionally records an event initiator
    https://www.agileplannerapp.com/boards/173808/cards/3442

--- app/models/event_log.rb
+++ app/models/event_log.rb
@@ -11,14 +11,23 @@ class EventLog < ActiveRecord::Base
   SUSPENDED_ACCOUNT_AUTHENTICATED_LOGIN = "Unsuccessful login attempt to a suspended account, with the correct username and password"
   UNSUCCESSFUL_PASSPHRASE_CHANGE = "Unsuccessful passphrase change"
 
+  EVENTS_REQUIRING_INITIATOR = [ACCOUNT_SUSPENDED, ACCOUNT_UNSUSPENDED, MANUAL_ACCOUNT_UNLOCK]
+
   validates :uid, presence: true
   validates :event, presence: true
+  validates_presence_of :initiator_id, if: Proc.new { |event_log| EVENTS_REQUIRING_INITIATOR.include? event_log.event }
 
-  def self.record_event(user, event)
-    EventLog.create(uid: user.uid, event: event)
+  belongs_to :initiator, class_name: "User"
+
+  def self.record_event(user, event, initiator = nil)
+    attributes = { uid: user.uid, event: event }
+    attributes.merge!(initiator_id: initiator.id) if initiator
+
+    EventLog.create(attributes)
   end
 
   def self.for(user)
     EventLog.order('created_at DESC').where(uid: user.uid).limit(100)
   end
+
 end
