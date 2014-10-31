9c6d771a3e78031f81ce0ff6401990bb1afc3c24
steventux <steve.laing@digital.cabinet-office.gov.uk>
2014-10-31 16:33:37 +0000

    Send an explanatory email when account is locked.
    Using sidekiq to deliver mails asynchronously.

--- test/integration/user_locking_test.rb
+++ test/integration/user_locking_test.rb
@@ -2,15 +2,32 @@ require 'test_helper'
  
 class UserLockingTest < ActionDispatch::IntegrationTest
   should "trigger if the user typed a wrong password too many times" do
+    Sidekiq::Testing.inline!
+    
     user = create(:user)
     visit root_path
     8.times { signin(email: user.email, password: "wrong password") }
 
     signin(user)
+
+    assert_equal user.email, last_email.to[0]
+    assert_equal "Your GOV.UK Signon account has been locked", last_email.subject
+
     assert_response_contains("Invalid email or passphrase.")
 
     user.reload
     assert user.access_locked?
+  end
+
+  should "enqueue the 'account locked' explanation email" do
+    sidekiq_job_count = Sidekiq::Extensions::DelayedMailer.jobs.size
+    user = create(:user)
+    visit root_path
+    8.times { signin(email: user.email, password: "wrong password") }
+
+    signin(user)
+
+    assert_equal sidekiq_job_count + 1, Sidekiq::Extensions::DelayedMailer.jobs.size
   end
 
   should "be reversible by admins" do
