67aa1b7ea6ee2c578d890b953ba3589a2effda5f
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-12-30 06:39:40 +0000

    Introduce Pundit for authorisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery
 
   before_filter do
     headers['X-Frame-Options'] = 'SAMEORIGIN'
   end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
@@ -11,6 +14,13 @@ class ApplicationController < ActionController::Base
 
   def current_resource_owner
     User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  private
+
+  def user_not_authorized(exception)
+    flash[:alert] = "You do not have permission to perform this action."
+    redirect_to root_path
   end
 
 end
