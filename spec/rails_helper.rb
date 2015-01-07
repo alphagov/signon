d7f044d86a2bc07d61508fe50398e4499231885b
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2015-01-07 08:53:17 +0000

    Fix Test::Unit interfering with rake spec
    for further information see:
    https://github.com/rspec/rspec-rails/issues/1171
    
    this was causing CI builds to fail.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,10 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+# prevent Test::Unit's AutoRunner from executing during RSpec's rake task
+# https://github.com/rspec/rspec-rails/issues/1171
+Test::Unit.run = true if defined?(Test::Unit) && Test::Unit.respond_to?(:run=)
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
