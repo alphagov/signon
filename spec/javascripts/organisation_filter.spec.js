4704708790155f62b9fccccdaad5059b74971c85
Paul Hayes <fofr@users.noreply.github.com>
2015-01-12 15:55:22 +0000

    Make long list of organisations searchable
    Use a custom js module to filter a long list of list items, hiding all
    but those that match the selected input.
    
    * Based on filterable table module in govuk_admin_template
    * When opening the filter, focus the text input
    * Avoid hiding the drop down when clicking within the text input
    * Lock the width of the drop down when filtering
    * Ignores the first item in the list

---
+++ spec/javascripts/organisation_filter.spec.js
@@ -0,0 +1,83 @@
+describe('An organisation filter module', function() {
+  "use strict";
+
+  var root = window,
+      organisationFilter,
+      listElement;
+
+  beforeEach(function() {
+    listElement = $('<div>\
+      <ul class="js-filter-list">\
+        <li>\
+          <form>\
+            <input type="text" class="js-filter-list-input">\
+          </form>\
+        </li>\
+        <li class="first">\
+          <a href="/first-link">something</a>\
+        </li>\
+        <li class="second">\
+          <a href="/second-link">another thing</a>\
+        </li>\
+      </ul>\
+    </div>');
+
+    $('body').append(listElement);
+    organisationFilter = new GOVUKAdmin.Modules.OrganisationFilter();
+    organisationFilter.start(listElement);
+  });
+
+  afterEach(function() {
+    listElement.remove();
+  });
+
+  it('filters the organisation list based on input', function() {
+    filterBy('another');
+    expect(listElement.find('.first').is(':visible')).toBe(false);
+    expect(listElement.find('.second').is(':visible')).toBe(true);
+
+    filterBy('something');
+    expect(listElement.find('.first').is(':visible')).toBe(true);
+    expect(listElement.find('.second').is(':visible')).toBe(false);
+
+    filterBy('thing');
+    expect(listElement.find('.first').is(':visible')).toBe(true);
+    expect(listElement.find('.second').is(':visible')).toBe(true);
+
+    filterBy('not a thing');
+    expect(listElement.find('.first').is(':visible')).toBe(false);
+    expect(listElement.find('.second').is(':visible')).toBe(false);
+  });
+
+  it('keeps the first list item visible, the filter input', function() {
+    filterBy('another');
+    expect(listElement.find('li:first').is(':visible')).toBe(true);
+
+    filterBy('');
+    expect(listElement.find('li:first').is(':visible')).toBe(true);
+
+    filterBy('not a thing');
+    expect(listElement.find('li:first').is(':visible')).toBe(true);
+  });
+
+  it('shows all items when no input is entered', function() {
+    filterBy('another');
+    filterBy('');
+    expect(listElement.find('.first').is(':visible')).toBe(true);
+    expect(listElement.find('.second').is(':visible')).toBe(true);
+  });
+
+  describe('when the form is submitted', function() {
+    it('opens the first visible link', function() {
+      spyOn(GOVUKAdmin, 'redirect');
+      filterBy('another');
+      listElement.find('form').trigger('submit');
+      expect(GOVUKAdmin.redirect).toHaveBeenCalledWith('/second-link');
+    });
+  });
+
+  function filterBy(value) {
+    listElement.find('input').val(value).trigger('change');
+  }
+
+});
