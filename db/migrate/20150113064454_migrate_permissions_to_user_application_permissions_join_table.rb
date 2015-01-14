b7ce4587ba3d7cca695760c37b62a41ab7fd5ca6
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2015-01-14 09:15:44 +0000

    Migration to populate user_application_permissions

---
+++ db/migrate/20150113064454_migrate_permissions_to_user_application_permissions_join_table.rb
@@ -0,0 +1,34 @@
+class MigratePermissionsToUserApplicationPermissionsJoinTable < ActiveRecord::Migration
+  def up
+    puts "Migrating #{Permission.count} permissions to user_application_permissions join table"
+    cache_supported_permissions
+
+    index = 0
+    Permission.find_each do |permission|
+      permission.permissions.each do |supported_permission_name|
+        _supported_permission = @cache[permission.application_id][supported_permission_name]
+        next unless _supported_permission
+
+        permission = UserApplicationPermission.new(user_id: permission.user_id,
+                                                   application_id: permission.application_id,
+                                                   supported_permission_id: _supported_permission.id,
+                                                   last_synced_at: permission.last_synced_at,
+                                                   created_at: permission.created_at,
+                                                   updated_at: permission.updated_at)
+        permission.save if permission.valid? # doesn't save duplicate permissions
+      end
+      print '.' if (index += 1) % 1000 == 0
+    end
+    puts ''
+    puts 'Done.'
+  end
+
+  # caching saves us about 30s
+  def cache_supported_permissions
+    @cache ||= {}
+    SupportedPermission.all.each do |supported_permission|
+      @cache[supported_permission.application_id] ||= {}
+      @cache[supported_permission.application_id][supported_permission.name] = supported_permission
+    end
+  end
+end
