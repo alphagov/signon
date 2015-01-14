0fdfa6917eab59214bb127fa8f45ab6596bb6b86
issyl0 <isabell@issyl0.co.uk>
2015-01-14 12:04:35 +0000

    Reword the passphrase reset "email sent" flash message
    - I reset my passphrase earlier and thought that the wording was a bit weird.
      With Paul's help, here's an attempt at changing it.

--- test/integration/passphrase_reset_test.rb
+++ test/integration/passphrase_reset_test.rb
@@ -4,7 +4,7 @@ require 'helpers/passphrase_support'
 class PassphraseResetTest < ActionDispatch::IntegrationTest
   include PassPhraseSupport
 
-  BLANKET_RESET_MESSAGE = "If your email exists on our database, you will receive a passphrase recovery link on your email"
+  BLANKET_RESET_MESSAGE = "If your email address is recognised, you’ll receive an email with instructions about how to reset your passphrase."
 
   should "reset a user's password and let them set a new one" do
     user = create(:user)
