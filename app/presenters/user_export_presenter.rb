ddf6ecb46fbcf2cda25112b37a1515469cb5d3af
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2015-01-16 14:45:21 +0000

    Export CSV of currently filtered users.
    Includes each user's permissions in all apps.

---
+++ app/presenters/user_export_presenter.rb
@@ -0,0 +1,41 @@
+class UserExportPresenter
+  attr_accessor :user, :applications
+
+  def initialize(user, applications)
+    @user = user
+    @applications = applications
+  end
+
+  def self.header_row(applications)
+    [
+      'Name',
+      'Email',
+      'Role',
+      'Organisation',
+      'Sign-in count',
+      'Last sign-in',
+      'Created',
+      'Status',
+    ].concat applications.map &:name
+  end
+
+  def row
+    [
+      user.name,
+      user.email,
+      user.role.humanize,
+      user.organisation.try(:name),
+      user.sign_in_count,
+      user.current_sign_in_at.try(:to_formatted_s, :db),
+      user.created_at.try(:to_formatted_s, :db),
+      user.status.humanize,
+    ].concat(app_permissions)
+  end
+
+  def app_permissions
+    applications.map do |application|
+      perm = user.permissions.detect { |p| p.application_id == application.id }
+      perm.permissions.join(', ') if perm
+    end
+  end
+end
