cd70804477b21b59e39185c6d91e639d99ea0e99
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-04-21 09:29:39 +0000

    Comment to give context to OrganisationsFetcher
    Also, prevent an unnecessarily repeated query.

--- lib/organisations_fetcher.rb
+++ lib/organisations_fetcher.rb
@@ -11,6 +11,8 @@ class OrganisationsFetcher
       update_or_create_organisation(organisation_data)
       organisation_relationships[organisation_data.details.slug] = child_organisation_slugs(organisation_data)
     end
+    # Now that any new organisations have been created and any slug changes
+    # have been applied, we can safely tie together organisations
     update_ancestry(organisation_relationships)
   rescue ActiveRecord::RecordInvalid => invalid
     raise "Couldn't save organisation #{invalid.record.slug} because: #{invalid.record.errors.full_messages.join(',')}"
@@ -39,12 +41,13 @@ private
 
   def update_ancestry(organisation_relationships)
     organisation_relationships.each do |organisation_slug, child_organisation_slugs|
+      parent = Organisation.find_by_slug(organisation_slug)
       Organisation.where(slug: child_organisation_slugs).map do |child_organisation|
         # TODO this ignores that organisations can have multiple parents. I think organisations will
         # end up with the parent that appears last in the API response(s).
         #
         # Transition app implements this correctly.
-        child_organisation.update_attributes!(parent: Organisation.find_by_slug(organisation_slug))
+        child_organisation.update_attributes!(parent: parent)
       end
     end
   end
