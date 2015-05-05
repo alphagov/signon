0b7eee8215ae0ee84adf474542c4177ab7eea810
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-05-05 11:25:08 +0100

    Re-add build_resource to InvitationsController
    devise_invitable still expects the build_resource method to exist on the
    DeviseController, but it was removed in Devise 3.0:
    
        https://github.com/plataformatec/devise/commit/8a93c340802c9674cfcc39eec12793bd63cf5776#diff-d87e90fea7bf5b74d9b8c0e27c1316ceL109
    
    There isn't a safe later version of devise_invitable which we can upgrade to
    now, so staying on 1.1.5 and adding this method back into this controller
    seems like the best way to get to Devise 3.0.
    
    The original build_resource method in Devise checked for an `unsafe` option,
    but we aren't using it and can't see it in devise_invitable, so we've added a
    simplified version of this method. We also use resource_params instead of raw
    params here, which have been sanitised.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -58,6 +58,12 @@ class InvitationsController < Devise::InvitationsController
     users_path
   end
 
+  # TODO: remove this method when we're on a version of devise_invitable which
+  # no longer expects it to exist (v1.2.1 onwards)
+  def build_resource
+    self.resource = resource_class.new(resource_params)
+  end
+
   def resource_params
     sanitised_params = UserParameterSanitiser.new(
       user_params: unsanitised_user_params,
