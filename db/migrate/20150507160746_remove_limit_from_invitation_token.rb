24bb49c8dffa00ab503f00f5dc009e71d9475193
Stuart Gale <bishboria@gmail.com>
2015-05-08 11:48:22 +0100

    Add invitation_created_at column to User model
    As per the requirements of the Devise Invitable upgrade.
    
    Devise Invitable upgrade from 1.1.5 to 1.3.0, and this column
    was added in version 1.2.0 as described:
    https://github.com/scambra/devise_invitable/blob/master/CHANGELOG#L11

---
+++ db/migrate/20150507160746_remove_limit_from_invitation_token.rb
@@ -0,0 +1,9 @@
+class RemoveLimitFromInvitationToken < ActiveRecord::Migration
+  def up
+    change_column :users, :invitation_token, :string, :limit => nil
+  end
+
+  def down
+    change_column :users, :invitation_token, :string, :limit => 60
+  end
+end
