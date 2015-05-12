fb249cfec54b4db5d958a0995d62c3067ff09bf6
Stuart Gale <bishboria@gmail.com>
2015-05-12 16:08:41 +0100

    Comment out `per_key` check

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,8 @@ class SessionsController < Devise::SessionsController
       timeout_callback.present?,
       timeout_callback.kind == :before,
       timeout_callback.raw_filter.is_a?(Proc),
-      timeout_callback.per_key == {:if=>[], :unless=>[]},
+      # FIXME: per_key doesn't work any more, find an alternative
+      # timeout_callback.per_key == {:if=>[], :unless=>[]},
     ]
     unless callback_checks.all?
       raise "Something is wrong with the timeout callback: aborting"
