bdc90770ed17ec14ecd78d1335ae3b943ccd0b71
Paul Bowsher <paul.bowsher@gmail.com>
2015-06-17 16:26:23 +0100

    Add excuse comment for secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+
+# NOTE: For Rails 4.2 this should be moved to secrets.yml. We're not doing that work during the
+# Rails 4.1.11 upgrade because deployment currently needs to simultaenously support the Rails 3.2.x
+# version of this, so we're purposefully kicking the can down the road.
 Signonotron2::Application.config.secret_key_base = '101615e3369d108c13f7182caf9bb988fc8f2d8d309ebd16d39b34bece4b4bd0944df576bfa2ff35984e7c447658cc25810540b50759c15c2b94f8ef26867a8a'
