ff3f8998e75dd8e4f0f1eafd2d6a25746c55defb
Paul Bowsher <paul.bowsher@gmail.com>
2015-07-29 12:13:05 +0100

    Check config with rubocop in jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -8,14 +8,11 @@ export USE_SIMPLECOV=true
 export RAILS_ENV=test
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
-# Lint changes introduced in this branch, but not for master
 if [[ ${GIT_BRANCH} != "origin/master" ]]; then
   bundle exec govuk-lint-ruby \
-    --diff \
-    --cached \
     --format html --out rubocop-${GIT_COMMIT}.html \
     --format clang \
-    app test lib spec
+    app test lib spec config
 fi
 
 bundle exec rake stats
