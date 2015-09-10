5484403f92e559200892ec164a76e2628f6ad406
Paul Hayes <fofr@users.noreply.github.com>
2015-09-10 11:46:01 +0100

    Move flash messages into partial and helper

---
+++ app/helpers/bootstrap_flash_helper.rb
@@ -0,0 +1,16 @@
+module BootstrapFlashHelper
+  def bootstrap_flash_message_keys
+    [:success, :info, :warning, :danger, :error, :notice, :alert].select { |k| flash[k].present? }
+  end
+
+  def bootstrap_flash_class(flash_key)
+    case flash_key
+    when :notice
+      "success"
+    when :error, :alert
+      "danger"
+    else
+      flash_key
+    end
+  end
+end
