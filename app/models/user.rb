b68526fe543049c0cb79c46a76418562bf016fad
Ben Lovell <benjamin.lovell@gmail.com>
2015-09-29 10:44:19 +0100

    Resolves new rubocop style violations

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable,
          :recoverable, :trackable,
-         :validatable, :timeoutable, :lockable,                # devise core model extensions
+         :validatable, :timeoutable, :lockable, # devise core model extensions
          :invitable,    # in devise_invitable gem
          :suspendable,  # in signonotron2/lib/devise/models/suspendable.rb
          :zxcvbnable,
