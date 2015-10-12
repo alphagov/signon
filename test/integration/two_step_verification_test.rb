afc39523a83c29a93d972fd5897617bf492581b2
Paul Bowsher <paul.bowsher@gmail.com>
2015-10-12 15:33:05 +0100

    Key the 2SV session cookie on a hash of the secret
    Adding a hash of the secret key into the signed 2SV session cookie
    means that if a user changes their 2SV device, they will need to enter
    their new code on subsequent logins on all devices.

--- test/integration/two_step_verification_test.rb
+++ test/integration/two_step_verification_test.rb
@@ -5,20 +5,21 @@ require 'helpers/passphrase_support'
 class TwoStepVerificationTest < ActionDispatch::IntegrationTest
   context "setting a 2SV code" do
     setup do
-      @secret = ROTP::Base32.random_base32
-      ROTP::Base32.stubs(random_base32: @secret)
+      @new_secret = ROTP::Base32.random_base32
+      @original_secret = ROTP::Base32.random_base32
+      ROTP::Base32.stubs(random_base32: @new_secret)
     end
 
     context "with an existing 2SV setup" do
       setup do
-        @user = create(:user, email: "jane.user@example.com", otp_secret_key: ROTP::Base32.random_base32)
+        @user = create(:user, email: "jane.user@example.com", otp_secret_key: @original_secret)
         visit new_user_session_path
         signin_with_2sv(@user)
         visit two_step_verification_path
       end
 
       should "show the TOTP secret and a warning" do
-        assert_response_contains "Enter the code manually: #{@secret}"
+        assert_response_contains "Enter the code manually: #{@new_secret}"
         assert_response_contains "Setting up a new phone will replace your existing one. You will only be able to sign in with your new phone."
       end
 
@@ -27,7 +28,7 @@ class TwoStepVerificationTest < ActionDispatch::IntegrationTest
         click_button "submit_code"
 
         assert_response_contains "Sorry that code didn’t work. Please try again."
-        assert_response_contains "Enter the code manually: #{@secret}"
+        assert_response_contains "Enter the code manually: #{@new_secret}"
         assert_equal 1, EventLog.where(event: EventLog::TWO_STEP_CHANGE_FAILED, uid: @user.uid).count
       end
 
@@ -35,8 +36,18 @@ class TwoStepVerificationTest < ActionDispatch::IntegrationTest
         enter_code
 
         assert_response_contains "2-step verification phone changed successfully"
-        assert_equal @secret, @user.reload.otp_secret_key
+        assert_equal @new_secret, @user.reload.otp_secret_key
         assert_equal 1, EventLog.where(event: EventLog::TWO_STEP_CHANGED, uid: @user.uid).count
+      end
+
+      should "require the code again on next login" do
+        enter_code
+
+        within("main") do
+          click_link "Sign out"
+        end
+
+        signin_with_2sv(@user)
       end
     end
 
@@ -49,7 +60,7 @@ class TwoStepVerificationTest < ActionDispatch::IntegrationTest
       end
 
       should "show the TOTP secret" do
-        assert_response_contains "Enter the code manually: #{@secret}"
+        assert_response_contains "Enter the code manually: #{@new_secret}"
       end
 
       should "reject an invalid code, reuse the secret and log the rejection" do
@@ -57,7 +68,7 @@ class TwoStepVerificationTest < ActionDispatch::IntegrationTest
         click_button "submit_code"
 
         assert_response_contains "Sorry that code didn’t work. Please try again."
-        assert_response_contains "Enter the code manually: #{@secret}"
+        assert_response_contains "Enter the code manually: #{@new_secret}"
         assert_equal 1, EventLog.where(event: EventLog::TWO_STEP_ENABLE_FAILED, uid: @user.uid).count
       end
 
@@ -65,7 +76,7 @@ class TwoStepVerificationTest < ActionDispatch::IntegrationTest
         enter_code
 
         assert_response_contains "2-step verification set up"
-        assert_equal @secret, @user.reload.otp_secret_key
+        assert_equal @new_secret, @user.reload.otp_secret_key
         assert_equal 1, EventLog.where(event: EventLog::TWO_STEP_ENABLED, uid: @user.uid).count
       end
     end
@@ -73,7 +84,7 @@ class TwoStepVerificationTest < ActionDispatch::IntegrationTest
 
   def enter_code
     Timecop.freeze do
-      fill_in "code", with: ROTP::TOTP.new(@secret).now
+      fill_in "code", with: ROTP::TOTP.new(@new_secret).now
       click_button "submit_code"
     end
   end
