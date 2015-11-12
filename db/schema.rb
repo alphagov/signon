e68f6cd9bca9181310132054667856ee99541cde
Ben Lovell <benjamin.lovell@gmail.com>
2015-11-12 12:44:03 +0000

    Log and persist events by ID
    Declare and persist events by identifiers, rather than by their string
    representations.
    
    These changes will allow the `event_id` and `event` attributes/columns
    to coexist until such time a migration is ran to convert the existing
    `event` column values to their respective `event_id`s.
    
    Some of the changes to support this:
    
    * Override `event` reader and attempt to map message via `EVENTS`
    * Add the `event_id` attribute for persistence
    * Default `event` to an empty string so it is no longer mandatory
    * Don't validate `event` when an `event_id` is provided
    * Fixup broken specs that referred to the `event` directly
    * Removed a redundant spec that was well covered elsewhere
    * Validate presence of `event_id`
    * Ensure `event_id` corresponds with `EVENTS` entry

--- db/schema.rb
+++ db/schema.rb
@@ -45,9 +45,9 @@ ActiveRecord::Schema.define(version: 20151112110911) do
   add_index "batch_invitations", ["outcome"], name: "index_batch_invitations_on_outcome", using: :btree
 
   create_table "event_logs", force: :cascade do |t|
-    t.string   "uid",              limit: 255, null: false
-    t.string   "event",            limit: 255, null: false
-    t.datetime "created_at",                   null: false
+    t.string   "uid",              limit: 255,              null: false
+    t.string   "event",            limit: 255, default: "", null: false
+    t.datetime "created_at",                                null: false
     t.integer  "initiator_id",     limit: 4
     t.integer  "application_id",   limit: 4
     t.string   "trailing_message", limit: 255
