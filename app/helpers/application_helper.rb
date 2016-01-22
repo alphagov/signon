43763945776bc03adf3afd013ba56d8a94d0f3e3
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2016-01-22 17:21:13 +0000

    Don't send security tokens in URLs to Google Analytics
    I haven't found a way of automatically testing this that doesn't mean changing
    govuk_admin_template to render the Google Analytics Javascript in environments
    other than Rails' production.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,23 @@ module ApplicationHelper
       edit_email_or_passphrase_user_path(current_user)
     end
   end
+
+  def flash_text_without_email_addresses(message)
+    text_message = strip_tags(message)
+
+    # redact email addresses so they aren't passed to GA
+    text_message.gsub(/[\S]+@[\S]+/, '[email]')
+  end
+
+  SENSITIVE_QUERY_PARAMETERS = %w{reset_password_token invitation_token}
+
+  def sensitive_query_parameters?
+    (request.query_parameters.keys & SENSITIVE_QUERY_PARAMETERS).any?
+  end
+
+  def sanitised_fullpath
+    uri = Addressable::URI.parse(request.fullpath)
+    uri.query_values = uri.query_values.reject { |key, _value| SENSITIVE_QUERY_PARAMETERS.include?(key) }
+    uri.to_s
+  end
 end
---
+++ test/units/helpers/application_helper_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test '#sensitive_query_parameters? returns false when no parameters in the URL' do
+    self.request = ActionDispatch::Request.new(Rack::MockRequest.env_for("/nothing-to-hide"))
+    refute sensitive_query_parameters?
+  end
+
+  test '#sensitive_query_parameters? returns false when no sensitive parameters in the URL' do
+    self.request = ActionDispatch::Request.new(Rack::MockRequest.env_for("/nothing-to-hide?share=witheveryone"))
+    refute sensitive_query_parameters?
+  end
+
+  test '#sensitive_query_parameters? returns true when there is a reset_password_token in the URL' do
+    self.request = ActionDispatch::Request.new(Rack::MockRequest.env_for("/secret-squirrel?reset_password_token=d1"))
+    assert sensitive_query_parameters?
+  end
+
+  test '#sensitive_query_parameters? returns true when there is a invitation_token in the URL' do
+    self.request = ActionDispatch::Request.new(Rack::MockRequest.env_for("/secret-squirrel?invitation_token=w1"))
+    assert sensitive_query_parameters?
+  end
+
+  test '#sanitised_fullpath returns the URL without the sensitive query parameters' do
+    self.request = ActionDispatch::Request.new(Rack::MockRequest.env_for("/secret-squirrel?invitation_token=w1&reset_password_token=d1&sharing=ok"))
+    assert_equal "/secret-squirrel?sharing=ok", sanitised_fullpath
+  end
+end
