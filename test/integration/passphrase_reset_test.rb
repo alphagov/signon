6447e2a19c5ba40a6385a1974d09b0258e665fae
David Henry <dw_henry@yahoo.com.au>
2016-03-17 10:44:02 +0000

    Make tests run agnostic of locale
    We have removed the GOV.UK specific text from the application so that we can use pension wise specific text on our server. This was done using a pension wise specific locale file.
    
    In order to test that everything works as expected and all locale text has been populated we want to be able to run the tests suite with our alternative locale file.
    
    As a result, I removed and GOV.UK specific text from the tests and used regular express matches instead.
    
    - Remove reference to "digital.cabinet-office"
    - Deal with from address having optional quotes
    around the name portion of the email if the name 
    contains any special characters.  In this case 
    the '.' in 'GOV.UK'

--- test/integration/passphrase_reset_test.rb
+++ test/integration/passphrase_reset_test.rb
@@ -45,7 +45,7 @@ class PassphraseResetTest < ActionDispatch::IntegrationTest
 
       open_email(user.email)
       assert current_email
-      assert_equal "noreply-signon-development@digital.cabinet-office.gov.uk", current_email.from[0]
+      assert_match /noreply-signon-development@.+\.gov\.uk/, current_email.from[0]
       assert_equal nil, last_email.reply_to[0]
       assert_equal "Reset passphrase instructions", current_email.subject
 
