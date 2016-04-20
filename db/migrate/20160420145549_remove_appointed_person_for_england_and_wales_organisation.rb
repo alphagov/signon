96e172081a3fe94cd421bed49ec9cc540af5fc3b
Murray Steele <muz@h-lame.com>
2016-04-20 15:34:54 +0000

    Migration to remove an org created in error

---
+++ db/migrate/20160420145549_remove_appointed_person_for_england_and_wales_organisation.rb
@@ -0,0 +1,13 @@
+class RemoveAppointedPersonForEnglandAndWalesOrganisation < ActiveRecord::Migration
+  def up
+    organistion = Organisation.find_by(slug: '')
+    if organistion.present?
+      raise "Unexpected users for #{organisation.title}" if organistion.users.count > 0
+      organistion.destroy
+    end
+  end
+
+  def down
+    # This org was created in error, there's no need for a down to re-instate it
+  end
+end
