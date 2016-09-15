430b429735a0a77ee405f556d2d62b27eafac4e1
Rosa Fox <rosafox89@gmail.com>
2016-09-15 16:00:40 +0000

    Grant user permissions for manuals publisher
    - Find users with permissions for specialist_publisher and grant them with those same permissions for manuals publisher

---
+++ lib/user_permission_migrator.rb
@@ -0,0 +1,23 @@
+class UserPermissionMigrator
+  def self.migrate(source:, target:)
+    source_app = Doorkeeper::Application.find_by!(name: source)
+    target_app = Doorkeeper::Application.find_by!(name: target)
+    source_app_supported_permissions = source_app.supported_permissions
+    target_app_supported_permissions = target_app.supported_permissions
+
+    permissions = source_app_supported_permissions.map do |permission|
+      [permission.name, target_app_supported_permissions.find_by!(name: permission.name)]
+    end
+
+    permission_mappings = permissions.to_h
+
+    User.all.each do |user|
+      if user.has_access_to?(source_app)
+        permissions = user.permissions_for(source_app)
+        permissions.each do |permission|
+          UserApplicationPermission.create user: user, application: target_app, supported_permission: permission_mappings[permission]
+        end
+      end
+    end
+  end
+end
