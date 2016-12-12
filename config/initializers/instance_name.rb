fe2c91ede52d3833d2ecbc62c1746adac87ad6f1
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-12-12 15:19:49 +0000

    Configure instance name from ENV

--- config/initializers/instance_name.rb
+++ config/initializers/instance_name.rb
@@ -1,10 +1,7 @@
-# This file is overwritten at deploy-time from alphagov-deployment. At the time
-# of writing, it isn't overwritten in production.
-
 # Human-friendly name for this signon instance. This is used when generating
 # reminder emails that need to disambiguate between instances.
 if Rails.env.development? || Rails.env.test?
   Rails.application.config.instance_name = "development"
 else
-  Rails.application.config.instance_name = nil
+  Rails.application.config.instance_name = ENV.fetch("INSTANCE_NAME", nil)
 end
