4201ac9a104c932ef1c2a36aa8156232f016ba5a
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2017-01-17 11:45:10 +0000

    Revert "Merge pull request #526 from alphagov/remove-share-sale-publisher"
    This reverts commit 185a1a0e34cab24b08837fbc3e1a7a4df1b5989c, reversing
    changes made to 8fd632c01b5d030ea86ab0f7672b03652ca2e7b6.

--- db/migrate/20170110171851_remove_share_sale_publisher.rb
+++
@@ -1,6 +0,0 @@
-class RemoveShareSalePublisher < ActiveRecord::Migration
-  def change
-    share_sale_publisher = Doorkeeper::Application.find_by(name: "Share Sale Publisher")
-    share_sale_publisher.destroy if share_sale_publisher
-  end
-end
