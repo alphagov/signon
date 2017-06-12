434fe492a640fdc24b5b2e60493fa924cd02653c
Murray Steele <muz@h-lame.com>
2017-06-12 15:21:55 +0000

    Stop org admins from viewing organisations
    Org admins used to be able to see a list of all orgs under their own orgs
    subtree, but since we removed the ability for them to edit those users or
    add those orgs to a user it doesn't make sense for them to see this list.
    Not least because this list is somewhat useless on it's own.

--- spec/policies/organisation_policy_spec.rb
+++ spec/policies/organisation_policy_spec.rb
@@ -4,11 +4,11 @@ describe OrganisationPolicy do
   subject { described_class }
 
   permissions :index? do
-    it "is forbidden only for normal users" do
+    it "is forbidden to organisation admins and normal users" do
       expect(subject).to permit(create(:superadmin_user), Organisation)
       expect(subject).to permit(create(:admin_user), Organisation)
-      expect(subject).to permit(create(:organisation_admin), Organisation)
 
+      expect(subject).not_to permit(create(:organisation_admin), Organisation)
       expect(subject).not_to permit(create(:user), Organisation)
     end
   end
@@ -19,15 +19,17 @@ describe OrganisationPolicy do
       expect(subject).to permit(create(:user_in_organisation, role: 'admin'), build(:organisation))
     end
 
-    it "allows organisation admins to assign a user only to organisations within their organisation subtree" do
+    it "is forbidden for organisation admins" do
       organisation_admin = create(:organisation_admin)
       admins_organisation = organisation_admin.organisation
       child_organisation = create(:organisation, parent_id: admins_organisation.id)
 
-      expect(subject).to permit(organisation_admin, admins_organisation)
-      expect(subject).to permit(organisation_admin, child_organisation)
-
+      # can't assign some random org
       expect(subject).not_to permit(organisation_admin, build(:organisation))
+      # can't assign the org they are an admin for
+      expect(subject).not_to permit(organisation_admin, admins_organisation)
+      # can't assign an org that is in the subtree of the one they are an admin for
+      expect(subject).not_to permit(organisation_admin, child_organisation)
     end
   end
 
@@ -70,13 +72,8 @@ describe OrganisationPolicy do
     context 'for org admins' do
       let(:user) { create(:organisation_admin, organisation: org_root_one) }
 
-      it 'includes only organisations within their orgs subtree' do
-        expect(resolved_scope).to include(org_root_one)
-        expect(resolved_scope).to include(org_root_one_child_one)
-        expect(resolved_scope).to include(org_root_one_child_two)
-        expect(resolved_scope).to include(org_root_one_grandchild_one)
-        expect(resolved_scope).not_to include(org_root_two)
-        expect(resolved_scope).not_to include(org_root_two_child_one)
+      it 'is empty' do
+        expect(resolved_scope).to be_empty
       end
     end
 
