a47188c467dd4124dc21c9b95ddfc9962ebad7a5
Murray Steele <muz@h-lame.com>
2017-07-11 12:18:17 +0000

    Introduce UserPermissionManageableApplicationPolicy
    This wraps up the code in UserControllerMethods#visible_applications for
    web users as a pundit policy to make it easier to reuse.  The policy
    doesn't have any permission methods on it, it just defines a scope.
    The name is slightly unweildy, but I struggled to come up with something
    shorter that didn't sound too generic.  It has been chosen to give a hint
    that it refers to the `manage_supported_permissions?` permission on the
    `UserPolicy`.  The scope gives us the list of applications that the
    current user is allowed to edit permissions of.  For example, a
    superadmin can edit permissions for all applications, and so can an admin
    user.  Org admins however can only edit the permissions of applications
    that allow delegation of permissions and that the org admin has access
    to themselves.  Normal users can't edit permissions for any apps.

---
+++ app/policies/user_permission_manageable_application_policy.rb
@@ -0,0 +1,39 @@
+# This policy controls which applications a given user is allowed to
+# manage the permissions that other users have for it
+class UserPermissionManageableApplicationPolicy
+  attr_reader :current_user
+
+  def initialize(current_user, _ = nil)
+    @current_user = current_user
+  end
+
+  def scope
+    PermissionGrantableApplicationPolicy::Scope.new(current_user).resolve
+  end
+
+  class Scope
+    attr_reader :current_user
+
+    def initialize(current_user, _ = nil)
+      @current_user = current_user
+    end
+
+    def resolve
+      if current_user.superadmin?
+        applications
+      elsif current_user.admin?
+        applications
+      elsif current_user.organisation_admin?
+        applications.can_signin(current_user).with_signin_delegatable
+      else
+        applications.none
+      end
+    end
+
+  private
+
+    def applications
+      ::Doorkeeper::Application.includes(:supported_permissions)
+    end
+  end
+end
