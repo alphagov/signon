5a7e2c7981954a4a12538ba02d5d3be068e8cfee
Murray Steele <muz@h-lame.com>
2017-07-14 08:19:02 +0000

    Extract SupportedPermissionParameterFilter from UserUpdater
    We have other classes that do things to params in lib, so it can live
    there.  No specs as it's already comprehensively covered by tests around
    it's caller: UserUpdater.

--- app/services/user_update.rb
+++ app/services/user_update.rb
@@ -24,47 +24,13 @@ class UserUpdate
 
 private
 
-  class SupportedPermissionsSanitizer
-    attr_reader :current_user, :user, :param_set
-    def initialize(current_user, user, param_set)
-      @current_user = current_user
-      @user = user
-      @param_set = param_set
-    end
-
-    def sanitized_supported_permission_ids
-      # any permissions not in "attempting_to_add" should be removed if we're
-      # allowed to manipulate them
-      allowed_to_be_removed = authorised_supported_permission_ids - attempting_to_set_supported_permission_ids
-      # any permissions in "attempting_to_add" should be added if they're in the
-      # set we're allowed to manipulate
-      allowed_to_be_added = attempting_to_set_supported_permission_ids & authorised_supported_permission_ids
-
-      (existing_supported_permission_ids - allowed_to_be_removed) | allowed_to_be_added
-    end
-
-  private
-
-    def authorised_supported_permission_ids
-      @authorised_supported_permissions ||= Pundit.policy_scope(current_user, SupportedPermission).pluck(:id).map(&:to_s)
-    end
-
-    def existing_supported_permission_ids
-      @existing_supported_permission_ids ||= user.supported_permission_ids.map(&:to_s)
-    end
-
-    def attempting_to_set_supported_permission_ids
-      @attempting_to_set_supported_permission_ids ||= param_set.fetch(:supported_permission_ids, []).map(&:to_s)
-    end
-  end
-
-  def sanitised_user_params
-    sanitizer = SupportedPermissionsSanitizer.new(current_user, user, user_params)
-    user_params.merge(supported_permission_ids: sanitizer.sanitized_supported_permission_ids)
+  def filtered_user_params
+    filter = SupportedPermissionParameterFilter.new(current_user, user, user_params)
+    user_params.merge(supported_permission_ids: filter.filtered_supported_permission_ids)
   end
 
   def update_user
-    user.update_attributes(sanitised_user_params)
+    user.update_attributes(filtered_user_params)
   end
 
   def record_permission_changes(old_permissions)
