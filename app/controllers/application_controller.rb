2b77afd70d00309007bd2dff8aba612c2758c6be
Murray Steele <muz@h-lame.com>
2017-08-15 14:12:40 +0000

    Add signin permission requirement to doorkeeper_authorize!
    For endpoints made available to oauth clients we make sure that the user
    that owns the access token used on the request has signin permission on
    the application making the request on their behalf.  To achieve this we
    customise the `doorkeeper_authorize!` method that doorkeeper provides to
    protect your controllers.  Our version calls super to let the default
    implementation take care of parsing tokens and doing the real work of
    authenticating the request.  Once this is complete we check that the
    user has signin permission on the applicaiton (we obtain the user and
    application from the access token).  If not we redirect to the signin
    required path, if they do we continue as before.
    
    It's not 100% clear to me that we need to do this.  All these requests
    will have been preceded by a call to the oauth endpoints to obtain the
    access token that is used in this request.  The oauth endpoints check
    that the user has signin permission on the application so it should not
    be possible to obtain an access token without this permission. However,
    I'm not sure how long lived our access tokens are, and by making sure
    each api request to signon checks this, we are able to invalidate
    requests as soon as the user's permission is revoked rather than waiting
    for their access token to expire.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,10 +21,36 @@ class ApplicationController < ActionController::Base
   end
 
   def current_resource_owner
-    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    @_current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  def application_making_request
+    @_application_making_request ||= ::Doorkeeper::Application.find(doorkeeper_token.application_id) if doorkeeper_token
   end
 
   private
+
+  def doorkeeper_authorize!
+    original_return_value = super
+    return original_return_value if api_user_via_token_has_signin_permission_on_app?
+    # The following code is a distillation of the error path of
+    # doorkeeper_authorize! from Doorkeeper::Rails::Helpers which is the
+    # super version called above.
+    options = doorkeeper_unauthorized_render_options
+    status = :unauthorized
+    if options.blank?
+      head status
+    else
+      options[:status] = status
+      options[:layout] = false if options[:layout].nil?
+      render options
+    end
+    original_return_value
+  end
+
+  def api_user_via_token_has_signin_permission_on_app?
+    current_resource_owner && application_making_request && current_resource_owner.has_access_to?(application_making_request)
+  end
 
   def user_not_authorized(exception)
     flash[:alert] = "You do not have permission to perform this action."
