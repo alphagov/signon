21e2667a2bd66a0b50167800a379f74a01ca409c
Murray Steele <muz@h-lame.com>
2017-10-20 13:59:21 +0000

    Model DfT hierarchy correctly
    For: https://trello.com/c/qk0lXJYW/260-model-dft-correctly-within-signon
    
    Checks that the children and grandchildren of the "Department for
    Transport" (DfT) have the right ancestry assigned to them.  We also make
    sure the children orgs that should be closed are closed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020125018) do
+ActiveRecord::Schema.define(version: 20171020132253) do
 
   create_table "batch_invitation_application_permissions", force: :cascade do |t|
     t.integer  "batch_invitation_id",     limit: 4, null: false
