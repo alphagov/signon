424953c279c46d1a5f86cdcc8910a1fad6074d2b
Murray Steele <muz@h-lame.com>
2017-11-07 13:17:50 +0000

    Model DfID org structure correctly
    For: https://trello.com/c/TzflLNHY/271-model-dfid-correctly-within-signon
    
    Checks if child and grandchild organisations of the "Department for
    International Development" (DfID) have the correct parent assigned. It
    also re-assigns some closed organisations to DfID and closes them if they
    are not already in the "closed" state.

---
+++ db/migrate/20171107131556_model_dfid_org_structure.rb
@@ -0,0 +1,68 @@
+class ModelDfidOrgStructure < ActiveRecord::Migration
+  def up
+    dfid = Organisation.find_by(name: "Department for International Development")
+
+    child_org_names = [
+      'Commonwealth Scholarship Commission in the UK',
+      'Independent Commission for Aid Impact',
+    ]
+
+    closed_child_org_names = [
+      'Crown Agents Holding and Realisation Board',
+      'Natural Resources Institute',
+    ]
+
+    missing_orgs = []
+
+    child_org_names.each do |child_name|
+      org = Organisation.find_by(name: child_name)
+      if org.nil?
+        missing_orgs << child_name
+        puts "!! Couldn't find #{child_name}"
+      else
+        update_parent(org, dfid)
+      end
+    end
+
+    closed_child_org_names.each do |child_name|
+      org = Organisation.find_by(name: child_name)
+      if org.nil?
+        missing_orgs << child_name
+        puts "!! Couldn't find #{child_name}"
+      else
+        update_parent(org, dfid)
+        if org.closed?
+          puts "#{child_name} already closed"
+        else
+          org.update_attributes(closed: true)
+          puts "Marking #{child_name} as closed"
+        end
+      end
+    end
+
+    if missing_orgs.empty?
+      puts "All organisations were found"
+    else
+      puts "Missing organisations: #{missing_orgs.join("\n")}"
+    end
+  end
+
+  def update_parent(org, parent)
+    if org.parent != parent
+      begin
+        old_parent_name = org.parent.nil?? "nil" : org.parent.name
+        puts "Checking parent for #{org.name}. Old parent is #{old_parent_name}"
+        org.update_attributes!(parent: parent)
+        puts "Updating parent for #{org.name} from #{old_parent_name} to #{parent.name}"
+      rescue => error
+        puts "Parent re-assignment failed for: #{org.name} with error '#{error.message}'"
+      end
+    else
+      puts "Parent for #{org.name} is correct"
+    end
+  end
+
+  def down
+    # This change cannot be reversed
+  end
+end
