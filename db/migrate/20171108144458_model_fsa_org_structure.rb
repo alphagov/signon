e8c9ea90704e2441c7fe89ba8357565de11c8356
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2017-11-08 16:36:55 +0000

    Model FSA org structure correctly
    For: https://trello.com/c/8MA3tUOe/276-model-fsa-correctly-within-signon
    
    Checks if child and grandchild organisations of the "Food Standards Agency" (FSA) have the correct parent assigned.
    It also re-assigns some closed organisations to FSA and closes them if they are not already in the "closed" state.

---
+++ db/migrate/20171108144458_model_fsa_org_structure.rb
@@ -0,0 +1,72 @@
+class ModelFsaOrgStructure < ActiveRecord::Migration
+  def up
+    fsa = Organisation.find_by(name: "Food Standards Agency")
+
+    child_org_names = [
+        "Advisory Committee on Animal Feedingstuffs",
+        "Advisory Committee on Novel Foods and Processes",
+        "Advisory Committee on the Microbiological Safety of Food",
+        "Committee on Toxicity of Chemicals in Food, Consumer Products and the Environment",
+        "General Advisory Committee on Science",
+        "Social Science Research Committee"
+    ]
+
+    closed_child_org_names = [
+        "British Potato Council",
+        "Meat Hygiene Service"
+    ]
+
+    missing_orgs = []
+
+    child_org_names.each do |child_name|
+      org = Organisation.find_by(name: child_name)
+      if org.nil?
+        missing_orgs << child_name
+        puts "!! Couldn't find #{child_name}"
+      else
+        update_parent(org, fsa)
+      end
+    end
+
+    closed_child_org_names.each do |child_name|
+      org = Organisation.find_by(name: child_name)
+      if org.nil?
+        missing_orgs << child_name
+        puts "!! Couldn't find #{child_name}"
+      else
+        update_parent(org, fsa)
+        if org.closed?
+          puts "#{child_name} already closed"
+        else
+          org.update_attributes(closed: true)
+          puts "Marking #{child_name} as closed"
+        end
+      end
+    end
+
+    if missing_orgs.empty?
+      puts "All organisations were found"
+    else
+      puts "Missing organisations: #{missing_orgs.join("\n")}"
+    end
+  end
+
+  def update_parent(org, parent)
+    if org.parent != parent
+      begin
+        old_parent_name = org.parent.nil?? "nil" : org.parent.name
+        puts "Checking parent for #{org.name}. Old parent is #{old_parent_name}"
+        org.update_attributes!(parent: parent)
+        puts "Updating parent for #{org.name} from #{old_parent_name} to #{parent.name}"
+      rescue => error
+        puts "Parent re-assignment failed for: #{org.name} with error '#{error.message}'"
+      end
+    else
+      puts "Parent for #{org.name} is correct"
+    end
+  end
+
+  def down
+    # This change cannot be reversed
+  end
+end
