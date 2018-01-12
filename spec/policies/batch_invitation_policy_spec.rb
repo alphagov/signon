cd35f090dd8d45e6bada8dda1c80b43f0227e189
Murray Steele <muz@h-lame.com>
2018-01-12 12:15:37 +0000

    Allow superadmins to upload CSVs of users with organisation info
    Only a superadmin is allowed to batch invite users and have separate orgs
    for each user in the batch.  We do this with a new permission on the
    BatchInvitationPolicy and ignoring any Organisation data in the CSV if the
    current user does not have that permission (currently only super admins
    do).

--- spec/policies/batch_invitation_policy_spec.rb
+++ spec/policies/batch_invitation_policy_spec.rb
@@ -5,12 +5,12 @@ describe BatchInvitationPolicy do
 
   permissions :new? do
     it 'allows superadmins and admins to create new batch uploads' do
-      expect(subject).to permit(create(:user_in_organisation, role: 'superadmin'), BatchInvitation.new)
-      expect(subject).to permit(create(:user_in_organisation, role: 'admin'), BatchInvitation.new)
+      expect(subject).to permit(create(:superadmin_user), BatchInvitation.new)
+      expect(subject).to permit(create(:admin_user), BatchInvitation.new)
     end
 
     it 'is forbidden for organisation admins to create new batch uploads even within their organisation subtree' do
-      organisation_admin = create(:user_in_organisation, role: 'organisation_admin')
+      organisation_admin = create(:organisation_admin)
 
       expect(subject).not_to permit(organisation_admin, BatchInvitation.new)
       expect(subject).not_to permit(organisation_admin, BatchInvitation.new(organisation_id: create(:organisation).id))
@@ -18,11 +18,33 @@ describe BatchInvitationPolicy do
     end
 
     it 'is forbidden for super organisation admins to create new batch uploads even within their organisation subtree' do
-      super_org_admin = create(:user_in_organisation, role: 'super_organisation_admin')
+      super_org_admin = create(:super_org_admin)
 
       expect(subject).not_to permit(super_org_admin, BatchInvitation.new)
       expect(subject).not_to permit(super_org_admin, BatchInvitation.new(organisation_id: create(:organisation).id))
       expect(subject).not_to permit(super_org_admin, BatchInvitation.new(organisation_id: super_org_admin.organisation_id))
+    end
+
+    it 'is forbidden for normal users' do
+      expect(subject).not_to permit(create(:user), BatchInvitation.new)
+    end
+  end
+
+  permissions :assign_organisation_from_csv? do
+    it 'is allowed for super admins' do
+      expect(subject).to permit(create(:superadmin_user), BatchInvitation.new)
+    end
+
+    it 'is forbidden for admins' do
+      expect(subject).not_to permit(create(:admin_user), BatchInvitation.new)
+    end
+
+    it 'is forbidden for super organisation admins' do
+      expect(subject).not_to permit(create(:super_org_admin), BatchInvitation.new)
+    end
+
+    it 'is forbidden for organisation admins' do
+      expect(subject).not_to permit(create(:organisation_admin), BatchInvitation.new)
     end
 
     it 'is forbidden for normal users' do
