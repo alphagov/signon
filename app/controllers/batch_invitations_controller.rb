760337340dd1592190ca632c6e73811efd5130aa
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-08-02 14:42:18 +0100

    Fix a warning about SQL injection by using parameters

--- app/controllers/batch_invitations_controller.rb
+++ app/controllers/batch_invitations_controller.rb
@@ -67,7 +67,7 @@ class BatchInvitationsController < ApplicationController
   private
 
   def recent_batch_invitations
-    @_recent_batch_invitations ||= BatchInvitation.where("created_at > '#{3.days.ago}'").order("created_at desc")
+    @_recent_batch_invitations ||= BatchInvitation.where("created_at > ?", 3.days.ago).order("created_at desc")
   end
 
   def file_uploaded?
