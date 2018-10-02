8c27ed61883cc6c3fa0936ba3f6eacd4da590236
Tijmen Brommet <tijmen@gmail.com>
2018-10-02 09:34:44 +0100

    Add navigation items to header

---
+++ app/helpers/component_helper.rb
@@ -0,0 +1,36 @@
+module ComponentHelper
+  def navigation_items
+    return [] unless current_user
+
+    items = []
+
+    items << { text: 'Dashboard', href: root_path, active: is_current?(root_path) }
+
+    if policy(User).index?
+      items << { text: 'Users', href: users_path, active: is_current?(users_path) }
+    end
+
+    if policy(ApiUser).index?
+      items << { text: 'API Users', href: api_users_path, active: is_current?(users_path) }
+    end
+
+    if policy(Doorkeeper::Application).index?
+      items << { text: 'Apps', href: doorkeeper_applications_path, active: is_current?(users_path) }
+    end
+
+    if policy(Organisation).index?
+      items << { text: 'Organisations', href: organisations_path, active: is_current?(users_path) }
+    end
+
+    items << { text: current_user.name, href: user_link_target }
+    items << { text: "Sign out", href: destroy_user_session_path }
+
+    items
+  end
+
+  def is_current?(link)
+    recognized = Rails.application.routes.recognize_path(link)
+    recognized[:controller] == params[:controller] &&
+      recognized[:action] == params[:action]
+  end
+end
