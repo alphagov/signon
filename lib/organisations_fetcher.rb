0d78b3a1810405e966f181e8a45279357b61f738
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-11-28 17:10:34 +0000

    Update Orgs api endpoint
    Orgs are now served from www.gov.uk.  The Whitehall endpoint is deprecated.

--- lib/organisations_fetcher.rb
+++ lib/organisations_fetcher.rb
@@ -20,7 +20,7 @@ class OrganisationsFetcher
 private
 
   def organisations
-    base_uri = Plek.find('whitehall-admin')
+    base_uri = Plek.new.website_root
     GdsApi::Organisations.new(base_uri).organisations.with_subsequent_pages
   end
 
