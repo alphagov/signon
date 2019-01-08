1df065e5868298f87b59316617d3741712fae731
Tijmen Brommet <tijmen@gmail.com>
2019-01-08 15:55:01 +0000

    Don't use `action_cable` and `active_storage`
    We're not using these frameworks. Removing them makes the app load
    faster.

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require 'action_controller/railtie'
 require 'action_view/railtie'
 require 'action_mailer/railtie'
 require 'active_job/railtie'
-require 'action_cable/engine'
-require 'active_storage/engine'
 require 'rails/test_unit/railtie'
 require 'sprockets/railtie'
 
