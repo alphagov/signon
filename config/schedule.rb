9f453022ee1d88c29061c17b00d1afb23de22805
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2019-05-03 13:15:29 +0100

    Set event log retention policy
    This commit enforces a retention policy of 2 years for signon event logs. This is achieved with a scheduled job that deletes all log entries older than 2 years every day.

---
+++ config/schedule.rb
@@ -0,0 +1,11 @@
+require 'whenever'
+
+# default cron env is "/usr/bin:/bin" which is not sufficient as govuk_setenv is in /usr/local/bin
+env :PATH, '/usr/local/bin:/usr/bin:/bin'
+
+set :output, error: 'log/cron.error.log', standard: 'log/cron.log'
+job_type :rake, "cd :path && govuk_setenv signon bundle exec rake :task :output"
+
+every 1.day, at: "1:15 am" do
+  rake "event_log:delete_logs_older_than_two_years"
+end
