a9e34e7213972c982f7e2d816d39e35ca04eb226
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-07-12 16:17:45 +0100

    Add APHA as a child of Defra
    This migration adds APHA to the list of children of Defra.
    
    See Zendesk: https://govuk.zendesk.com/agent/tickets/3738715

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_105421) do
-
+ActiveRecord::Schema.define(version: 2019_07_12_160951) do
+  
   create_table "batch_invitation_application_permissions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "batch_invitation_id", null: false
     t.integer "supported_permission_id", null: false
