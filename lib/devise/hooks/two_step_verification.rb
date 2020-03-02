ef29048034b8a782716cb494de26c695f83ada62
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2020-03-02 11:44:44 +0000

    Namespace Devise two-step verification

--- lib/devise/hooks/two_step_verification.rb
+++ lib/devise/hooks/two_step_verification.rb
@@ -1,12 +1,14 @@
-Warden::Manager.after_authentication do |user, auth, _options|
-  if user.need_two_step_verification?
-    cookie = auth.env["action_dispatch.cookies"].signed["remember_2sv_session"]
-    valid = cookie &&
-      cookie["user_id"] == user.id &&
-      cookie["valid_until"] > Time.zone.now &&
-      cookie["secret_hash"] == Digest::SHA256.hexdigest(user.otp_secret_key)
-    unless valid
-      auth.session(:user)["need_two_step_verification"] = user.need_two_step_verification?
+module Devise::Hooks::TwoStepVerification
+  Warden::Manager.after_authentication do |user, auth, _options|
+    if user.need_two_step_verification?
+      cookie = auth.env["action_dispatch.cookies"].signed["remember_2sv_session"]
+      valid = cookie &&
+        cookie["user_id"] == user.id &&
+        cookie["valid_until"] > Time.zone.now &&
+        cookie["secret_hash"] == Digest::SHA256.hexdigest(user.otp_secret_key)
+      unless valid
+        auth.session(:user)["need_two_step_verification"] = user.need_two_step_verification?
+      end
     end
   end
 end
