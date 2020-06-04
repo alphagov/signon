ce23fc339921a0275f78b35ac2b64cfb5749a10e
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-06-04 13:49:39 +0100

    Fix Rails/HelperInstanceVariable issues
    This also renames one of the helper methods so its usages are clearer,
    and deletes the associated (but empty) test file.

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -1,13 +1,13 @@
 module RootHelper
-  def gds_only_application_and_non_gds_user?
-    @application.gds_only? && !current_user.belongs_to_gds?
+  def gds_only_application_and_non_gds_user?(application)
+    application.gds_only? && !current_user.belongs_to_gds?
   end
 
-  def title
-    if @application.blank? || gds_only_application_and_non_gds_user?
+  def signin_required_title(application)
+    if application.blank? || gds_only_application_and_non_gds_user?(application)
       "You don’t have permission to use this app."
     else
-      "You don’t have permission to sign in to #{@application.name}."
+      "You don’t have permission to sign in to #{application.name}."
     end
   end
 end
