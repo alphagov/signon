5bd8e85643a5bc151dd6bfe2d666993865bef36c
Thomas Leese <thomas@leese.io>
2021-01-15 11:22:19 +0000

    Refactor iterator methods
    This updates the code to take advantage of `each_with_object` and
    `find_each`.

--- lib/organisations_fetcher.rb
+++ lib/organisations_fetcher.rb
@@ -5,10 +5,9 @@ require "gds_api/organisations"
 # membership of organisations.
 class OrganisationsFetcher
   def call
-    organisation_relationships = {}
-    organisations.each do |organisation_data|
+    organisation_relationships = organisations.each_with_object({}) do |organisation_data, memo|
       update_or_create_organisation(organisation_data)
-      organisation_relationships[organisation_data["details"]["slug"]] = child_organisation_slugs(organisation_data)
+      memo[organisation_data["details"]["slug"]] = child_organisation_slugs(organisation_data)
     end
     # Now that any new organisations have been created and any slug changes
     # have been applied, we can safely tie together organisations
@@ -39,17 +38,18 @@ private
       abbreviation: organisation_data["details"]["abbreviation"],
       closed: organisation_data["details"]["govuk_status"] == "closed",
     }
+
     organisation.update!(update_data)
   end
 
   def child_organisation_slugs(organisation_data)
-    organisation_data["child_organisations"].collect { |child_organisation| child_organisation["id"].split("/").last }
+    organisation_data["child_organisations"].map { |child_organisation| child_organisation["id"].split("/").last }
   end
 
   def update_ancestry(organisation_relationships)
     organisation_relationships.each do |organisation_slug, child_organisation_slugs|
-      parent = Organisation.find_by(slug: organisation_slug)
-      Organisation.where(slug: child_organisation_slugs).map do |child_organisation|
+      parent = Organisation.find_by!(slug: organisation_slug)
+      Organisation.where(slug: child_organisation_slugs).find_each do |child_organisation|
         # TODO: this ignores that organisations can have multiple parents. I think organisations will
         # end up with the parent that appears last in the API response(s).
         #
