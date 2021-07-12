47002fa7e274f1709f2a18ea4dedfae9c87fdb19
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-07-12 13:23:00 +0100

    Point to Rails 6.1 defaults
    Also delete the temporary 'new_framework_defaults' files for
    6.0 and 6.1.

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ Bundler.require(*Rails.groups)
 module Signon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     config.active_record.belongs_to_required_by_default = false
 
