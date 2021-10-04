29146294056e9494f79cac969302d25a8f974cf9
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2021-10-04 15:55:24 +0100

    Bugfix: new MFA device can be created without current MFA token
    Note: This fix has already been deployed via the private repo
    deployment method.
    
    A user could access the paths handled by Devise::TwoStepVerificationController
    without entering their current MFA token if it existed.
    
    This enabled a user to bypass the MFA flow.
    
    Steps to reproduce provided by in-house pentester:
    
    1. Enter username and password: https://signon.staging.publishing.service.gov.uk/
    2. You're presented with the MFA form
    3. Go directly to https://signon.staging.publishing.service.gov.uk/users/two_step_verification in your browser
    4. You can now replace MFA for the account without needing theirs
    
    Fix:
    
    By removing the skip_before_action: :handle_two_step_verification
    we require a user to have entered their existing MFA token before
    changing their MFA device.
    
    Commit 0ecdf332938239f6243ac86e02c820b8c34d4c49 seems to have
    introduced this in September 2015.

--- test/controllers/two_step_verification_controller_test.rb
+++ test/controllers/two_step_verification_controller_test.rb
@@ -9,13 +9,26 @@ class TwoStepVerificationControllerTest < ActionController::TestCase
     sign_in @user
   end
 
-  context "when unauthenticated" do
+  context "when user is not logged in" do
     setup { sign_out @user }
 
     should "redirect to login upon attempted prompt" do
       get :prompt
 
       assert_redirected_to new_user_session_path
+    end
+  end
+
+  context "when MFA code is required by login journey" do
+    setup do
+      sign_out @user
+      sign_in @user, passed_mfa: false
+    end
+
+    should "redirect to login upon attempted prompt" do
+      get :prompt
+
+      assert_redirected_to new_two_step_verification_session_path
     end
   end
 
