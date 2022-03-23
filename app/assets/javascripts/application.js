63cd62a25e3d1f6781921976afeaf71950fa35fc
Kevin Dew <kevindew@me.com>
2022-03-23 20:15:57 +0000

    Switch from jquery_ujs to rails-ujs
    This has been done with the expectation that jQuery will soon be removed
    from the govuk_publishing_components and that we will need to use the
    vanilla JS equivalent of Rails unobtrusive JavaScript.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/all_components
 //= require password-strength-indicator
-//= require jquery_ujs
+//= require rails-ujs
