0cefd9d924015c1325541bd5d1885f1cdf590a89
Kevin Dew <kevindew@me.com>
2022-03-29 13:36:50 +0100

    Add node_modules folder to the asset load path
    This is so the bundled version of jQuery can be picked up.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,5 +6,5 @@ Rails.application.config.assets.version = "1.0"
 # Enable the asset pipeline
 Rails.application.config.assets.enabled = true
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+# Add node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
