4bff041a0f3b7f22713bfb2326d8e605a3ad1c67
Kevin Dew <kevindew@me.com>
2022-03-29 13:32:03 +0100

    Switch to using an assets manifest for Sprockets 4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,13 +8,3 @@ Rails.application.config.assets.enabled = true
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-Rails.application.config.assets.precompile += %w[
-  legacy_layout.css
-  legacy_layout.js
-  password-strength-indicator.js
-  print.css
-]
