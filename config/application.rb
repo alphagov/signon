e711982abbf560804d5db76a9bb1085be8f93d66
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-04-13 11:50:43 +0100

    Rotate cookies for Rails 7 upgrade
    Rails 7 now changes the default digest class for the key generator from
    SHA1 to SHA256.
    
    We therefore need to rotate cookies for existing user sessions.

--- config/application.rb
+++ config/application.rb
@@ -76,5 +76,22 @@ module Signon
     # to use CSS that has same function names as SCSS such as max.
     # https://github.com/alphagov/govuk-frontend/issues/1350
     config.assets.css_compressor = nil
+
+    # Rotate SHA1 cookies to SHA256 (the new Rails 7 default)
+    # TODO: Remove this after existing user sessions have been rotated
+    # https://guides.rubyonrails.org/v7.0/upgrading_ruby_on_rails.html#key-generator-digest-class-changing-to-use-sha256
+    config.action_dispatch.cookies_rotations.tap do |cookies|
+      salt = Rails.application.config.action_dispatch.authenticated_encrypted_cookie_salt
+      secret_key_base = Rails.application.secrets.secret_key_base
+      next if secret_key_base.blank?
+
+      key_generator = ActiveSupport::KeyGenerator.new(
+        secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
+      )
+      key_len = ActiveSupport::MessageEncryptor.key_len
+      secret = key_generator.generate_key(salt, key_len)
+
+      cookies.rotate :encrypted, secret
+    end
   end
 end
