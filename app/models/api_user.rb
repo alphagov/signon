11b70305156e4a1dbf67fa5b1b9e4e5b5ec55fe1
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2023-02-03 11:24:13 +0000

    Ensure api users cannot require 2sv or have exemption reasons
    2sv does not apply to api users, so we should ensure that the fields relating to requiring 2sv or being exempt from it cannot be set. This should prevent us from requiring 2sv for application to application communication, including for external api users.
    
    In the future, where we switch on 2sv for all users except those with exemption reasons, we may want to change the exemption reason validation for api users to ensure that it is always present, rather than always absent, and use this as the mechanism by which we do not set 2sv on these users. For now, however, we require that a 2sv exemption reason is not set to avoid getting into odd states, and as part of ensuring that api users are not exemptable via the UI.

--- app/models/api_user.rb
+++ app/models/api_user.rb
@@ -1,6 +1,18 @@
 class ApiUser < User
   default_scope { where(api_user: true).order(:name) }
+  validate :reason_for_2sv_exemption_blank
+  validate :require_2sv_is_false
 
   DEFAULT_TOKEN_LIFE = 2.years.to_i
   AUTOROTATABLE_TOKEN_LIFE = 60.days.to_i
+
+private
+
+  def require_2sv_is_false
+    errors.add(:require_2sv, "can't be true for api user") if require_2sv
+  end
+
+  def reason_for_2sv_exemption_blank
+    errors.add(:reason_for_2sv_exemption, "can't be present for api user") if reason_for_2sv_exemption.present?
+  end
 end
