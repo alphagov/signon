b3d09398e3c10cf6b4ca94101bceeb9058f84f76
Chris Lowis <chris.lowis@digital.cabinet-office.gov.uk>
2023-06-26 14:24:55 +0100

    Simplify tests for bulk granting sigin permission
    This page now only allows the granting of a single permission (signin)
    for a single application, so the tests can be simplified to reflect
    that change.

--- test/integration/bulk_granting_permisions_test.rb
+++ test/integration/bulk_granting_permisions_test.rb
@@ -15,21 +15,13 @@ class BulkGrantingPermissionsTest < ActionDispatch::IntegrationTest
   should "superadmin user can grant multiple permissions to all users in one go" do
     user = create(:superadmin_user)
 
-    permissions = {
-      @application => %w[signin],
-    }
-
-    perform_bulk_grant_as_user(user, permissions)
+    perform_bulk_grant_as_user(user, @application)
   end
 
   should "admin user can grant multiple permissions to all users in one go" do
     user = create(:admin_user)
 
-    permissions = {
-      @application => %w[signin],
-    }
-
-    perform_bulk_grant_as_user(user, permissions)
+    perform_bulk_grant_as_user(user, @application)
   end
 
   should "super organisation admin user can not grant multiple permissions to all users in one go" do
@@ -62,42 +54,28 @@ class BulkGrantingPermissionsTest < ActionDispatch::IntegrationTest
     assert_equal root_path, current_path
   end
 
-  def perform_bulk_grant_as_user(acting_user, permissions)
+  def perform_bulk_grant_as_user(acting_user, application)
     perform_enqueued_jobs do
       visit root_path
       signin_with(acting_user)
 
       visit new_bulk_grant_permission_set_path
 
-      select permissions.keys.first.name, from: "Application"
+      select application.name, from: "Application"
 
       click_button "Grant access to all users"
 
-      assert_response_contains("Scheduled grant of #{permissions.map { |_app, perms| perms.count }.inject(:+)} permissions to all users")
+      assert_response_contains("Scheduled grant of 1 permissions to all users")
       assert_response_contains("Granting permissions to all users")
       assert_response_contains("All #{User.all.count} users processed")
 
-      permissions.each do |application, app_permissions|
-        app_permissions_line = "#{application.name} "
-        app_permissions_line <<
-          if app_permissions.include? "signin"
-            "Yes"
-          else
-            "No"
-          end
-        assert_response_contains app_permissions_line
-      end
+      app_permissions_line = "#{application.name} Yes"
+      assert_response_contains app_permissions_line
 
       [acting_user, @users, @org_admins, @admins, @superadmins].flatten.each do |user|
         user.reload
-        assert_has_permissions user, permissions
+        assert_equal %w[signin], user.permissions_for(application)
       end
-    end
-  end
-
-  def assert_has_permissions(user, permissions)
-    permissions.each do |application, app_permissions|
-      assert_equal app_permissions.sort, user.permissions_for(application).sort
     end
   end
 end
