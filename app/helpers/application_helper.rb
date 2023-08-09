bb981a525d4b273a4bfbff9de1e35c829adff264
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-08-09 13:55:17 +0100

    Add user research recruitment banner
    Trello: https://trello.com/c/yGMU3sR2
    
    We're only showing the banner on the dashboard so that we don't
    overwhelm people by showing it everywhere.
    
    Ideally we'd have used the GOV.UK Components button [1] for the two
    buttons. However, the `govuk-button--inverse` CSS class [2] is not
    supported by the component and there's no easy way to override it.
    Since we're not using that component we've had to introduce
    `ApplicationHelper#arrow_svg` to generate the arrow SVG for the
    start-style button [3]. It would be worth opening a PR to add an
    `inverse` property to the GOV.UK Components button so we can avoid this
    duplication.
    
    [1]: https://components.publishing.service.gov.uk/component-guide/button
    [2]: https://design-system.service.gov.uk/components/button/#buttons-on-dark-backgrounds
    [3]:
    https://design-system.service.gov.uk/components/button/#start-buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,4 +35,16 @@ module ApplicationHelper
     uri.query_values = uri.query_values.reject { |key, _value| SENSITIVE_QUERY_PARAMETERS.include?(key) }
     uri.to_s
   end
+
+  def start_button_text(text)
+    "#{text} #{arrow_svg}".html_safe
+  end
+
+  def arrow_svg
+    %(
+      <svg class="govuk-button__start-icon" xmlns="http://www.w3.org/2000/svg" width="17.5" height="19" viewBox="0 0 33 40" aria-hidden="true" focusable="false">
+        <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
+      </svg>
+    )
+  end
 end
