bf4af439b7bc09ba591c3d9d01f396e66b07eecb
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-08-09 14:23:02 +0100

    Hide user research recruitment banner permanently
    Trello: https://trello.com/c/yGMU3sR2
    
    If the user clicks on "Find out more" then we assume that they've
    completed the Google Form and therefore we don't want to annoy them by
    continuing to show them the recruitment banner.
    
    To achieve this we set
    User#current_user.user_research_recruitment_banner_hidden to true before
    sending them off to the Google Form.

--- app/controllers/user_research_recruitment_controller.rb
+++ app/controllers/user_research_recruitment_controller.rb
@@ -1,9 +1,15 @@
 class UserResearchRecruitmentController < ApplicationController
+  USER_RESEARCH_RECRUITMENT_FORM_URL = "https://docs.google.com/forms/d/1Bdu_GqOrSR4j6mbuzXkFTQg6FRktRMQc8Y-q879Mny8/viewform".freeze
+
   before_action :authenticate_user!
   skip_after_action :verify_authorized
 
   def update
-    if params[:choice] == "dismiss-banner"
+    case params[:choice]
+    when "participate"
+      current_user.update!(user_research_recruitment_banner_hidden: true)
+      redirect_to USER_RESEARCH_RECRUITMENT_FORM_URL, allow_other_host: true
+    when "dismiss-banner"
       cookies[:dismiss_user_research_recruitment_banner] = true
       redirect_to root_path
     end
--- test/controllers/user_research_recruitment_controller_test.rb
+++ test/controllers/user_research_recruitment_controller_test.rb
@@ -4,15 +4,15 @@ class UserResearchRecruitmentControllerTest < ActionController::TestCase
   attr_reader :choice
 
   context "#update" do
+    should "require users to be signed in" do
+      put :update
+
+      assert_redirected_to new_user_session_path
+    end
+
     context "when user clicks the button to dismiss the banner" do
       setup do
         @choice = "dismiss-banner"
-      end
-
-      should "require signed in users" do
-        put :update, params: { choice: }
-
-        assert_redirected_to new_user_session_path
       end
 
       should "set session cookie" do
@@ -29,6 +29,29 @@ class UserResearchRecruitmentControllerTest < ActionController::TestCase
         put :update, params: { choice: }
 
         assert_redirected_to root_path
+      end
+    end
+
+    context "when user clicks the button to participate in user research" do
+      setup do
+        @choice = "participate"
+      end
+
+      should "set user_research_recruitment_banner_hidden to true for the current_user" do
+        user = create(:user)
+        sign_in user
+
+        put :update, params: { choice: }
+
+        assert user.user_research_recruitment_banner_hidden?
+      end
+
+      should "redirect to the Google Form" do
+        sign_in create(:user)
+
+        put :update, params: { choice: }
+
+        assert_redirected_to UserResearchRecruitmentController::USER_RESEARCH_RECRUITMENT_FORM_URL
       end
     end
   end
