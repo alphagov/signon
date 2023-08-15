a8cb5303916664342a16c1c1f6544e7644913cb4
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-08-15 11:55:04 +0100

    Make MailHelper#app_name consistent with #email_from_address
    In MailHelper#email_from_address and other places we do NOT include the
    word "production" to distinguish which GOV.UK environment (integration,
    staging, production) the app is running in.
    
    So it seems more consistent and less confusing to the user NOT to
    include the word "production" in MailHelper#app_name.

--- app/mailers/mailer_helper.rb
+++ app/mailers/mailer_helper.rb
@@ -4,7 +4,11 @@ module MailerHelper
   end
 
   def app_name
-    I18n.t("mailer.app_name", instance_name: GovukEnvironment.name)
+    if GovukEnvironment.production?
+      I18n.t("mailer.app_name.no_instance")
+    else
+      I18n.t("mailer.app_name.instance", instance_name: GovukEnvironment.name)
+    end
   end
 
   def email_from_address
