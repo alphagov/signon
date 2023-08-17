2a79ea803cc1338e1b9a79b15c1562e33ab6a44e
Chris Roos <chris@seagul.co.uk>
2023-08-17 12:37:41 +0100

    Add test to exercise OAuth2 flow
    This test demonstrates/exercises the OAuth2 interactions involved in the
    Authorization Code Grant type[1]. This is the "happy path" that's
    followed when the user has the signin permission in the application
    they're authorizing to make requests on their behalf.
    
    I'm adding this to give me confidence that I'm not breaking this
    behaviour when I enable the PKCE extension.
    
    Note that some of our "integration" tests are using Rails'
    RequestHelpers[2] (e.g `get` and `post`) while others are using the
    Capybara methods (e.g. `visit`). I'm using the Rails RequestHelpers as I
    need to simulate requests that happen outside of the browser (e.g.  the
    request for the Access Token).
    
    Note that CSRF protection was enabled in our integration tests in
    c69288d1516714ba2c4fe6ad30e0f4f18bc17bf7. It's not particularly useful
    in these tests so I'm disabling it rather than working out how to
    get/set the authenticity token.
    
    I'm using `ActionDispatch::Integration::Runner#reset!`[3] to reset the
    session between the user approving the Authorization Grant (in the form
    of the Authorization Code) and the client using that Authorization Grant
    to request an Access Token. My hope is that this makes it slightly
    clearer that the Authorization Code request is performed by the user's
    User Agent and the Access Token request, and subsequent request for the
    protected resource, is performed by the client.
    
    [1]: https://oauth.net/2/grant-types/authorization-code/
    [2]: https://api.rubyonrails.org/v7.0.7/classes/ActionDispatch/Integration/RequestHelpers.html
    [3]: https://api.rubyonrails.org/v7.0.7/classes/ActionDispatch/Integration/Runner.html#method-i-reset-21

--- test/integration/doorkeeper_integration_test.rb
+++ test/integration/doorkeeper_integration_test.rb
@@ -6,4 +6,58 @@ class DoorkeeperIntegrationTest < ActionDispatch::IntegrationTest
   rescue ActionController::RoutingError => e
     assert_equal 'No route matches [GET] "/oauth/applications"', e.message
   end
+
+  test "oauth2 authorization code grant type" do
+    without_csrf_protection do
+      app = create(:application)
+      password = SecureRandom.urlsafe_base64
+      user = create(:user, with_signin_permissions_for: [app], password:)
+
+      sign_in user.email, password
+
+      auth_code = request_authorization_code(app)
+
+      reset!
+
+      access_token = request_access_token(app, auth_code)
+
+      user_data = request_user_data(app, access_token)
+      assert_equal user.uid, user_data["user"]["uid"]
+    end
+  end
+
+private
+
+  def without_csrf_protection
+    original_value = ActionController::Base.allow_forgery_protection
+    ActionController::Base.allow_forgery_protection = false
+    yield
+  ensure
+    ActionController::Base.allow_forgery_protection = original_value
+  end
+
+  def sign_in(email, password)
+    post user_session_path, params: { user: { email:, password: } }
+    assert_redirected_to root_path
+  end
+
+  def request_authorization_code(app)
+    get oauth_authorization_path, params: { response_type: "code", client_id: app.uid, redirect_uri: app.redirect_uri }
+    assert_response :redirect
+    Rack::Utils.parse_query(URI.parse(response.location).query)["code"]
+  end
+
+  def request_access_token(app, auth_code)
+    http_basic_auth = ActionController::HttpAuthentication::Basic.encode_credentials(app.uid, app.secret)
+    post oauth_token_path, params: { grant_type: "authorization_code", code: auth_code, redirect_uri: app.redirect_uri },
+                           headers: { Authorization: http_basic_auth }
+    assert_response :success
+    JSON.parse(response.body)["access_token"]
+  end
+
+  def request_user_data(app, access_token)
+    get "/user.json", params: { client_id: app.uid }, headers: { Authorization: "Bearer #{access_token}" }
+    assert_response :success
+    JSON.parse(response.body)
+  end
 end
