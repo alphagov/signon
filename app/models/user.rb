7b0c2f8490d9ab5d1c4614673efdcb0a64c20c91
Mike Patrick <mike.patrick@digital.cabinet-office.gov.uk>
2023-08-24 10:28:17 +0100

    Scope for never signed in Users that have expired
    We're planning to start deleting unused user accounts. But "unused" is
    hard to define. And it's unclear what potential impacts this might have
    on the publishing apps that those users have accessed.
    
    So, here's a definition of unused user accounts that definitely haven't
    accessed any publishing apps! Users that were invited more than 90 days
    ago but have never signed in.
    
    We've chosen 90 days because
    
    1. 90's round-ish number, as numbers-of-days go
    2. it's comfortably longer than the 2 weeks that invitations expire after
    3. it sounds like a generous enough amount of time for a new user and
       their inviter can go back and forth, attempting to get signed in,
       resending the invitation, etc
    
    Since it appears that invitations can be resent, this query is using
    `invitation_sent_at` (instead of, e.g. `created_at`) to give all
    invitations the same grace period.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   self.include_root_in_json = true
 
+  NEVER_SIGNED_IN_EXPIRY_PERIOD = 90.days
+
   SUSPENSION_THRESHOLD_PERIOD = 45.days
   UNSUSPENSION_GRACE_PERIOD = 7.days
 
@@ -66,6 +68,8 @@ class User < ApplicationRecord
   scope :last_signed_in_on, ->(date) { web_users.not_suspended.where("date(current_sign_in_at) = date(?)", date) }
   scope :last_signed_in_before, ->(date) { web_users.not_suspended.where("date(current_sign_in_at) < date(?)", date) }
   scope :last_signed_in_after, ->(date) { web_users.not_suspended.where("date(current_sign_in_at) >= date(?)", date) }
+  scope :never_signed_in, -> { web_users.where(current_sign_in_at: nil) }
+  scope :expired_never_signed_in, -> { never_signed_in.where("invitation_sent_at < ?", NEVER_SIGNED_IN_EXPIRY_PERIOD.ago) }
   scope :not_recently_unsuspended, -> { where(["unsuspended_at IS NULL OR unsuspended_at < ?", UNSUSPENSION_GRACE_PERIOD.ago]) }
   scope :with_access_to_application, ->(application) { UsersWithAccess.new(self, application).users }
   scope :with_2sv_enabled,
