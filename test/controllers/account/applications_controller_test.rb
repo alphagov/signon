defd38936b39cd904bc025bb0d0c6665b9c6194a
Chris Roos <chris@seagul.co.uk>
2023-09-21 12:52:57 +0100

    Don't display "Grant access" button to Publishing Managers
    Publishing Managers aren't allowed to grant themselves access to
    applications so we shouldn't show them this button.

--- test/controllers/account/applications_controller_test.rb
+++ test/controllers/account/applications_controller_test.rb
@@ -22,4 +22,18 @@ class Account::ApplicationsControllerTest < ActionController::TestCase
       assert_redirected_to "/account/applications"
     end
   end
+
+  context "#index" do
+    context "logged in as a publishing manager" do
+      should "not display the button to grant access to an application" do
+        application = create(:application, name: "app-name")
+        sign_in create(:organisation_admin_user)
+
+        get :index
+
+        assert_select "tr td", text: "app-name"
+        assert_select "form[action='#{account_application_signin_permission_path(application)}']", count: 0
+      end
+    end
+  end
 end
