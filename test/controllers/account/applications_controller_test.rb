036a2ee546ea6f3a08bedfa1641fbe68574f1d7e
Chris Roos <chris@seagul.co.uk>
2023-09-21 14:30:49 +0100

    Conditionally display the "Remove access" button
    Publishing Managers can only remove their access from applications that
    have delegatable permissions. We should only display the button if
    they're allowed to remove their access.

--- test/controllers/account/applications_controller_test.rb
+++ test/controllers/account/applications_controller_test.rb
@@ -34,6 +34,19 @@ class Account::ApplicationsControllerTest < ActionController::TestCase
         assert_select "tr td", text: "app-name"
         assert_select "form[action='#{account_application_signin_permission_path(application)}']", count: 0
       end
+
+      should "not display the button to remove access to an application" do
+        application = create(:application, name: "app-name")
+        application.signin_permission.update!(delegatable: false)
+        user = create(:organisation_admin_user, with_signin_permissions_for: [application])
+
+        sign_in user
+
+        get :index
+
+        assert_select "tr td", text: "app-name"
+        assert_select "a[href='#{delete_account_application_signin_permission_path(application)}']", count: 0
+      end
     end
   end
 end
