5b4f7f51d9a69f1b7a495cf96a1cd419d5fb368f
Mike Patrick <mike.patrick@digital.cabinet-office.gov.uk>
2023-09-22 16:57:23 +0100

    Link to Manage permissions from Account page

--- test/integration/account_test.rb
+++ test/integration/account_test.rb
@@ -8,7 +8,21 @@ class AccountTest < ActionDispatch::IntegrationTest
       assert_current_url new_user_session_path
     end
 
-    should "link to Change email/password" do
+    should "link to Change email/password and Manage permissions for admin users" do
+      user = FactoryBot.create(:admin_user)
+
+      visit new_user_session_path
+      signin_with user
+
+      visit account_path
+      assert_current_url account_path
+      assert page.has_selector?("h1", text: "Settings")
+
+      assert page.has_link?("Change your email or password", href: account_email_password_path)
+      assert page.has_link?("Manage permissions", href: account_manage_permissions_path)
+    end
+
+    should "link to Change email/password for normal users" do
       user = FactoryBot.create(:user)
 
       visit new_user_session_path
@@ -18,6 +32,8 @@ class AccountTest < ActionDispatch::IntegrationTest
       assert page.has_selector?("h1", text: "Settings")
 
       assert page.has_link?("Change your email or password", href: account_email_password_path)
+
+      assert_not page.has_link?("Manage permissions")
     end
   end
 end
