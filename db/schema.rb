90b3cfdaf8af3ded7b9dea146c13ff702185210c
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-09-25 11:51:39 +0100

    Strip whitespace from name on existing User records
    In #2373 I noticed that the query we'd used to do this in #2291 didn't
    escape the regular expression properly. ActiveRecord seems to have
    generated the following SQL WHERE expression:
    
        WHERE (name REGEXP('^ +') OR name REGEXP(' +$')
    
    I _think_ this will only have found names with leading/trailing
    space characters and not leading/trailing
    newlines/carriage-returns/tabs. Thus the previous migration didn't do
    anything bad, it just missed _some_ records.
    
    In this new version of the migration I've constructed the query slightly
    differently. Firstly I've double-escaped the whitespace character in the
    regular expression and secondly I've used an Array Condition vs a Pure
    String Condition [1] to ensure argument safety. The new query generates
    the following SQL WHERE expression:
    
        WHERE (name REGEXP '^\\s+' OR name REGEXP '\\s+$')
    
    This has the REGEXP string argument escaped correctly.
    
    As per the migrations in #2373 I've use
    ActiveRecord::Persistence#update_attribute in order to skip model
    validation, in case some of the existing User records are invalid for
    other reasons.
    
    As before I did consider writing the migration as a SQL UPDATE, but
    generating a TRIM expression that is exactly equivalent to String#strip
    isn't as trivial as it sounds!
    
    [1]: https://guides.rubyonrails.org/active_record_querying.html#conditions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_093143) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_104847) do
   create_table "batch_invitation_application_permissions", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.integer "batch_invitation_id", null: false
     t.integer "supported_permission_id", null: false
