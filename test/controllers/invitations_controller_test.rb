d8c3f1e8a31dbfcf6836aecf9e1ed45dc1d7f1b5
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-09-28 12:52:51 +0100

    Rename invitee params in InvitationsControllerTest
    This is another small step in trying to make the test easy to read by
    being clear that the parameters relate to the invitee and not the
    inviter.
    
    I've also made the domain used for all email addresses consistently used
    gov.uk, because the differences were distracting me.

--- test/controllers/invitations_controller_test.rb
+++ test/controllers/invitations_controller_test.rb
@@ -57,12 +57,12 @@ class InvitationsControllerTest < ActionController::TestCase
 
       should "disallow access to non-admin inviter" do
         @inviter.update_column(:role, "normal")
-        post :create, params: { user: { name: "Testing Non-admins", email: "testing_non_admins@example.com" } }
+        post :create, params: { user: { name: "invitee", email: "invitee@gov.uk" } }
         assert_redirected_to root_path
       end
 
       should "not allow creation of api users" do
-        post :create, params: { user: { name: "Testing APIs", email: "api@example.com", api_user: true } }
+        post :create, params: { user: { name: "api-invitee", email: "api-invitee@gov.uk", api_user: true } }
 
         assert_empty User.where(api_user: true)
       end
@@ -78,91 +78,91 @@ class InvitationsControllerTest < ActionController::TestCase
 
       should "disallow access to organisation admin inviter" do
         @inviter.update!(role: Roles::OrganisationAdmin.role_name, organisation_id: create(:organisation).id)
-        post :create, params: { user: { name: "Testing Org Admins", email: "testing_org_admins@example.com" } }
+        post :create, params: { user: { name: "org-admin-invitee", email: "org-admin-invitee@gov.uk" } }
         assert_redirected_to root_path
       end
 
       should "disallow access to super organisation admin inviter" do
         @inviter.update!(role: Roles::SuperOrganisationAdmin.role_name, organisation_id: create(:organisation).id)
-        post :create, params: { user: { name: "Testing Org Admins", email: "testing_org_admins@example.com" } }
+        post :create, params: { user: { name: "super-org-admin-invitee", email: "super-org-admin-invitee@gov.uk" } }
         assert_redirected_to root_path
       end
 
       should "save invitee and render 2SV form when invitee will be assigned to organisation that does not require 2SV" do
         organisation = create(:organisation, require_2sv: false)
 
-        post :create, params: { user: { name: "User Name", email: "person@gov.uk", organisation_id: organisation.id } }
+        post :create, params: { user: { name: "invitee", email: "invitee@gov.uk", organisation_id: organisation.id } }
 
         assert_redirected_to require_2sv_user_path(User.last)
-        assert_equal "User Name", User.last.name
+        assert_equal "invitee", User.last.name
       end
 
       should "save invitee and not render 2SV form when invitee will be assigned to organisation that requires 2SV" do
         organisation = create(:organisation, require_2sv: true)
 
-        post :create, params: { user: { name: "User Name", email: "person@gov.uk", organisation_id: organisation.id } }
+        post :create, params: { user: { name: "invitee", email: "invitee@gov.uk", organisation_id: organisation.id } }
 
         assert_redirected_to users_path
-        assert_equal "User Name", User.last.name
+        assert_equal "invitee", User.last.name
       end
 
       should "not render 2SV form and saves invitee when invitee will be a superadmin" do
         organisation = create(:organisation, require_2sv: false)
 
-        post :create, params: { user: { name: "User Name", email: "person@gov.uk", organisation_id: organisation.id, role: Roles::Superadmin.role_name } }
+        post :create, params: { user: { name: "superadmin-invitee", email: "superadmin-invitee@gov.uk", organisation_id: organisation.id, role: Roles::Superadmin.role_name } }
 
         assert_redirected_to users_path
-        assert_equal "User Name", User.last.name
+        assert_equal "superadmin-invitee", User.last.name
         assert User.last.require_2sv
       end
 
       should "not render 2SV form and saves invitee when invitee will be an admin" do
         organisation = create(:organisation, require_2sv: false)
 
-        post :create, params: { user: { name: "User Name", email: "person@gov.uk", organisation_id: organisation.id, role: Roles::Admin.role_name } }
+        post :create, params: { user: { name: "admin-invitee", email: "admin-invitee@gov.uk", organisation_id: organisation.id, role: Roles::Admin.role_name } }
 
         assert_redirected_to users_path
-        assert_equal "User Name", User.last.name
+        assert_equal "admin-invitee", User.last.name
         assert User.last.require_2sv
       end
 
       should "not render 2SV form and saves invitee when invitee will be an organisation admin" do
         organisation = create(:organisation, require_2sv: false)
 
-        post :create, params: { user: { name: "User Name", email: "person@gov.uk", organisation_id: organisation.id, role: Roles::OrganisationAdmin.role_name } }
+        post :create, params: { user: { name: "org-admin-invitee", email: "org-admin-invitee@gov.uk", organisation_id: organisation.id, role: Roles::OrganisationAdmin.role_name } }
 
         assert_redirected_to users_path
-        assert_equal "User Name", User.last.name
+        assert_equal "org-admin-invitee", User.last.name
         assert User.last.require_2sv
       end
 
       should "not render 2SV form and saves invitee when invitee will be a super organisation admin" do
         organisation = create(:organisation, require_2sv: false)
 
-        post :create, params: { user: { name: "User Name", email: "person@gov.uk", organisation_id: organisation.id, role: Roles::SuperOrganisationAdmin.role_name } }
+        post :create, params: { user: { name: "super-org-admin-invitee", email: "super-org-admin-invitee@gov.uk", organisation_id: organisation.id, role: Roles::SuperOrganisationAdmin.role_name } }
 
         assert_redirected_to users_path
-        assert_equal "User Name", User.last.name
+        assert_equal "super-org-admin-invitee", User.last.name
         assert User.last.require_2sv
       end
 
       should "re-render form and not save invitee if there are validation errors" do
-        post :create, params: { user: { name: "User Name", email: "" } }
+        post :create, params: { user: { name: "invitee-without-email", email: "" } }
 
         assert_template :new
-        assert_not User.exists?(name: "User Name")
+        assert_not User.exists?(name: "invitee-without-email")
       end
 
       should "record account invitation in event log when invitation sent" do
         EventLog.expects(:record_account_invitation).with(instance_of(User), @inviter)
 
-        post :create, params: { user: { name: "User Name", email: "person@gov.uk" } }
+        post :create, params: { user: { name: "invitee", email: "invitee@gov.uk" } }
       end
 
       should "not record account invitation in event log when invitation not sent because of validation errors" do
         EventLog.expects(:record_account_invitation).never
 
-        post :create, params: { user: { name: "User Name", email: "" } }
+        post :create, params: { user: { name: "invitee-without-email", email: "" } }
       end
 
       should "not record account invitation in event log when invitation not sent because user already exists" do
