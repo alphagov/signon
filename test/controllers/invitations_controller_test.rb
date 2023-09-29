da87bc38f0f3d89a459ad4bd2dbe23c6aeafe9d5
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-09-29 12:15:00 +0100

    Re-order tests in InvitationsControllerTest
    Move the "happy path" tests to the top of each context. I think it's
    more useful to prioritise these as they characterise the most
    significant behaviour of the controller.

--- test/controllers/invitations_controller_test.rb
+++ test/controllers/invitations_controller_test.rb
@@ -6,51 +6,7 @@ class InvitationsControllerTest < ActionController::TestCase
   end
 
   context "GET new" do
-    context "when inviter is not signed in" do
-      should "require inviter to be signed in" do
-        get :new
-
-        assert_redirected_to new_user_session_path
-      end
-    end
-
     context "when inviter is signed in" do
-      context "and inviter is not an admin" do
-        setup do
-          sign_in create(:user)
-        end
-
-        should "disallow access" do
-          get :new
-
-          assert_redirected_to root_path
-        end
-      end
-
-      context "and inviter is an organisation admin" do
-        setup do
-          sign_in create(:organisation_admin_user)
-        end
-
-        should "disallow access" do
-          get :new
-
-          assert_redirected_to root_path
-        end
-      end
-
-      context "and inviter is a super organisation admin" do
-        setup do
-          sign_in create(:super_organisation_admin_user)
-        end
-
-        should "disallow access" do
-          get :new
-
-          assert_redirected_to root_path
-        end
-      end
-
       context "and inviter is an admin" do
         setup do
           sign_in create(:admin_user)
@@ -74,23 +30,6 @@ class InvitationsControllerTest < ActionController::TestCase
           assert_template :new
         end
       end
-    end
-  end
-
-  context "POST create" do
-    context "when inviter is not signed in" do
-      should "require inviter to be signed in" do
-        post :create
-
-        assert_redirected_to new_user_session_path
-      end
-    end
-
-    context "when inviter is signed in" do
-      setup do
-        @inviter = create(:superadmin_user)
-        sign_in @inviter
-      end
 
       context "and inviter is not an admin" do
         setup do
@@ -98,25 +37,10 @@ class InvitationsControllerTest < ActionController::TestCase
         end
 
         should "disallow access" do
-          post :create, params: { user: { name: "invitee", email: "invitee@gov.uk" } }
+          get :new
 
           assert_redirected_to root_path
         end
-      end
-
-      should "not allow creation of api users" do
-        post :create, params: { user: { name: "api-invitee", email: "api-invitee@gov.uk", api_user: true } }
-
-        assert_empty User.where(api_user: true)
-      end
-
-      should "redirect to users page and display flash alert when inviting an existing user" do
-        existing_user = create(:user)
-
-        post :create, params: { user: { name: existing_user.name, email: existing_user.email } }
-
-        assert_redirected_to users_path
-        assert_equal "User already invited. If you want to, you can click 'Resend signup email'.", flash[:alert]
       end
 
       context "and inviter is an organisation admin" do
@@ -125,7 +49,7 @@ class InvitationsControllerTest < ActionController::TestCase
         end
 
         should "disallow access" do
-          post :create, params: { user: { name: "org-admin-invitee", email: "org-admin-invitee@gov.uk" } }
+          get :new
 
           assert_redirected_to root_path
         end
@@ -137,10 +61,27 @@ class InvitationsControllerTest < ActionController::TestCase
         end
 
         should "disallow access" do
-          post :create, params: { user: { name: "super-org-admin-invitee", email: "super-org-admin-invitee@gov.uk" } }
+          get :new
 
           assert_redirected_to root_path
         end
+      end
+    end
+
+    context "when inviter is not signed in" do
+      should "require inviter to be signed in" do
+        get :new
+
+        assert_redirected_to new_user_session_path
+      end
+    end
+  end
+
+  context "POST create" do
+    context "when inviter is signed in" do
+      setup do
+        @inviter = create(:superadmin_user)
+        sign_in @inviter
       end
 
       should "save invitee and render 2SV form when invitee will be assigned to organisation that does not require 2SV" do
@@ -227,23 +168,81 @@ class InvitationsControllerTest < ActionController::TestCase
 
         post :create, params: { user: { name: existing_user.name, email: existing_user.email } }
       end
-    end
-  end
 
-  context "POST resend" do
+      should "not allow creation of api users" do
+        post :create, params: { user: { name: "api-invitee", email: "api-invitee@gov.uk", api_user: true } }
+
+        assert_empty User.where(api_user: true)
+      end
+
+      should "redirect to users page and display flash alert when inviting an existing user" do
+        existing_user = create(:user)
+
+        post :create, params: { user: { name: existing_user.name, email: existing_user.email } }
+
+        assert_redirected_to users_path
+        assert_equal "User already invited. If you want to, you can click 'Resend signup email'.", flash[:alert]
+      end
+
+      context "and inviter is not an admin" do
+        setup do
+          sign_in create(:user)
+        end
+
+        should "disallow access" do
+          post :create, params: { user: { name: "invitee", email: "invitee@gov.uk" } }
+
+          assert_redirected_to root_path
+        end
+      end
+
+      context "and inviter is an organisation admin" do
+        setup do
+          sign_in create(:organisation_admin_user)
+        end
+
+        should "disallow access" do
+          post :create, params: { user: { name: "org-admin-invitee", email: "org-admin-invitee@gov.uk" } }
+
+          assert_redirected_to root_path
+        end
+      end
+
+      context "and inviter is a super organisation admin" do
+        setup do
+          sign_in create(:super_organisation_admin_user)
+        end
+
+        should "disallow access" do
+          post :create, params: { user: { name: "super-org-admin-invitee", email: "super-org-admin-invitee@gov.uk" } }
+
+          assert_redirected_to root_path
+        end
+      end
+    end
+
     context "when inviter is not signed in" do
       should "require inviter to be signed in" do
-        user_to_resend_for = create(:user)
-        post :resend, params: { id: user_to_resend_for.id }
+        post :create
 
         assert_redirected_to new_user_session_path
       end
     end
+  end
 
+  context "POST resend" do
     context "when inviter is signed in" do
       setup do
         @user_to_resend_for = create(:user)
         sign_in create(:superadmin_user)
+      end
+
+      should "resend account signup email to invitee" do
+        User.any_instance.expects(:invite!).once
+
+        post :resend, params: { id: @user_to_resend_for.id }
+
+        assert_redirected_to users_path
       end
 
       context "and inviter is not an admin" do
@@ -281,13 +280,14 @@ class InvitationsControllerTest < ActionController::TestCase
           assert_redirected_to root_path
         end
       end
+    end
 
-      should "resend account signup email to invitee" do
-        User.any_instance.expects(:invite!).once
+    context "when inviter is not signed in" do
+      should "require inviter to be signed in" do
+        user_to_resend_for = create(:user)
+        post :resend, params: { id: user_to_resend_for.id }
 
-        post :resend, params: { id: @user_to_resend_for.id }
-
-        assert_redirected_to users_path
+        assert_redirected_to new_user_session_path
       end
     end
   end
