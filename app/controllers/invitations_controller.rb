cc50bed0a5b1926d51491e1668189cc9f93382ee
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-10-03 17:52:14 +0100

    Use #resource vs @user in InvitationsController#create
    This seems more consistent with the code in the rest of this action.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -25,7 +25,7 @@ class InvitationsController < Devise::InvitationsController
     self.resource = resource_class.invite!(all_params, current_inviter)
     if resource.errors.empty?
       grant_default_permissions(resource)
-      EventLog.record_account_invitation(@user, current_user)
+      EventLog.record_account_invitation(resource, current_user)
       set_flash_message :notice, :send_instructions, email: resource.email
       respond_with resource, location: after_invite_path_for(resource)
     else
