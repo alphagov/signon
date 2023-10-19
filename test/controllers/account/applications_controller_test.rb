d3ee5233d0020bc00c65f60bd5b6af46b00ee483
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-10-19 11:53:34 +0100

    Add tests for GOV.UK admins to Account::ApplicationsControllerTest
    While I think there is effectively coverage of this behaviour in the
    `AccountApplicationsTest`, I found it confusing and potentially
    problematic that the controller test only included tests with negative
    assertions. I think it's important to always have tests that have the
    equivalent positive assertions nearby - otherwise the negative
    assertions might just be passing by accident.  Also in this case, I
    think it makes it more obvious that the behaviour differs depending on
    whether you are a GOV.UK admin or a Publishing Manager.

--- test/controllers/account/applications_controller_test.rb
+++ test/controllers/account/applications_controller_test.rb
@@ -24,6 +24,31 @@ class Account::ApplicationsControllerTest < ActionController::TestCase
   end
 
   context "#index" do
+    context "logged in as a GOV.UK admin" do
+      should "display the button to grant access to an application" do
+        application = create(:application, name: "app-name")
+        sign_in create(:admin_user)
+
+        get :index
+
+        assert_select "tr td", text: "app-name"
+        assert_select "form[action='#{account_application_signin_permission_path(application)}']"
+      end
+
+      should "display the button to remove access to an application" do
+        application = create(:application, name: "app-name")
+        application.signin_permission.update!(delegatable: false)
+        user = create(:admin_user, with_signin_permissions_for: [application])
+
+        sign_in user
+
+        get :index
+
+        assert_select "tr td", text: "app-name"
+        assert_select "a[href='#{delete_account_application_signin_permission_path(application)}']"
+      end
+    end
+
     context "logged in as a publishing manager" do
       should "not display the button to grant access to an application" do
         application = create(:application, name: "app-name")
