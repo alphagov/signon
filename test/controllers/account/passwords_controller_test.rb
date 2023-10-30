63ea5d06aa3fc2fb8863af8158bb54b92185d8e8
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-10-30 16:57:11 +0000

    Make Account::PasswordsControllerTest more idiomatic
    I found this test which was added over 11 years ago [1] very confusing.
    In particular I think there was too much going on in the
    `#change_user_password` method. While the new version introduces a
    little duplication, I think it's a bit easier to follow.
    
    [1]: https://github.com/alphagov/signon/commit/fb569a6b049a9313ab57c16c9fde09b496fbfb38

--- test/controllers/account/passwords_controller_test.rb
+++ test/controllers/account/passwords_controller_test.rb
@@ -1,52 +1,49 @@
 require "test_helper"
 
 class Account::PasswordsControllerTest < ActionController::TestCase
-  def change_user_password(user_factory, new_password)
-    original_password = "I am a very original password. Refrigerator weevil."
-    user = create(user_factory, password: original_password)
-    original_password_hash = user.encrypted_password
-    sign_in user
-
-    post :update,
-         params: {
-           user: {
-             current_password: original_password,
-             password: new_password,
-             password_confirmation: new_password,
-           },
-         }
-
-    [user, original_password_hash]
+  setup do
+    @original_password = "bf3b2cc3bb659ad6e740533b06c0b899"
+    @user = create(:user, password: @original_password)
+    sign_in @user
   end
 
   context "PUT update" do
-    should "changing passwords to something strong should succeed" do
-      user, orig_password = change_user_password(:user, "destabilizers842}orthophosphate")
+    should "update password if password is sufficiently strong" do
+      new_password = "0871feaffef29223358cbf086b4084c4"
+      put :update, params: { user: {
+        current_password: @original_password,
+        password: new_password,
+        password_confirmation: new_password,
+      } }
 
       assert_redirected_to account_path
-
-      user.reload
-      assert_not_equal orig_password, user.encrypted_password
+      assert @user.reload.valid_password?(new_password)
     end
 
-    should "changing password to something too short should fail" do
-      user, orig_password = change_user_password(:user, "short")
+    should "not update password if password is too short" do
+      new_password = "short"
+      put :update, params: { user: {
+        current_password: @original_password,
+        password: new_password,
+        password_confirmation: new_password,
+      } }
 
-      assert_equal "200", response.code
-      assert_match "too short", response.body
-
-      user.reload
-      assert_equal orig_password, user.encrypted_password
+      assert_template :edit
+      assert_select ".govuk-error-summary", text: /Password is too short/
+      assert @user.reload.valid_password?(@original_password)
     end
 
-    should "changing password to something too weak should fail" do
-      user, orig_password = change_user_password(:user, "zymophosphate")
+    should "not update password if password is too weak" do
+      new_password = "zymophosphate"
+      put :update, params: { user: {
+        current_password: @original_password,
+        password: new_password,
+        password_confirmation: new_password,
+      } }
 
-      assert_equal "200", response.code
-      assert_match "not strong enough", response.body
-
-      user.reload
-      assert_equal orig_password, user.encrypted_password
+      assert_template :edit
+      assert_select ".govuk-error-summary", text: /Password not strong enough/
+      assert @user.reload.valid_password?(@original_password)
     end
   end
 end
