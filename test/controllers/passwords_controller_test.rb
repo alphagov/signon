6db92b4092c0bd9f760831818348d9ed645e268c
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-10-31 12:03:49 +0000

    Restructure PasswordsControllerTest
    Group the tests for the `edit` action into a context.

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -7,19 +7,21 @@ class PasswordsControllerTest < ActionController::TestCase
     @token_received_in_email = @user.__send__(:send_reset_password_instructions)
   end
 
-  test "GET /edit with a bad reset token shows an error page" do
-    get :edit, params: { id: @user.id, reset_password_token: "not_a_real_token" }
+  context "GET edit" do
+    should "show an error page if password reset token is invalid" do
+      get :edit, params: { id: @user.id, reset_password_token: "not_a_real_token" }
 
-    assert_response :success
-    assert_template "devise/passwords/reset_error"
-  end
+      assert_response :success
+      assert_template "devise/passwords/reset_error"
+    end
 
-  test "GET /edit with an expired reset token shows an error page" do
-    @user.update!(reset_password_sent_at: 1.year.ago)
+    should "show an error page if password reset token has expired" do
+      @user.update!(reset_password_sent_at: 1.year.ago)
 
-    get :edit, params: { reset_password_token: @token_received_in_email }
+      get :edit, params: { reset_password_token: @token_received_in_email }
 
-    assert_response :success
-    assert_template "devise/passwords/reset_error"
+      assert_response :success
+      assert_template "devise/passwords/reset_error"
+    end
   end
 end
