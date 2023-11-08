39b1a364c0a1a55d07155ecc0f9a1cc26d89bd18
Chris Lowis <chris.lowis@digital.cabinet-office.gov.uk>
2023-11-08 11:30:09 +0000

    Extract application into Account::ApplicationsControllerTest setup
    Refactoring this test will allow me to add additional assertions in a
    more obvious manner in a subsequent commit.

--- test/controllers/account/applications_controller_test.rb
+++ test/controllers/account/applications_controller_test.rb
@@ -70,27 +70,29 @@ class Account::ApplicationsControllerTest < ActionController::TestCase
     end
 
     context "logged in as a publishing manager" do
+      setup do
+        @application = create(:application, name: "app-name")
+      end
+
       should "not display the button to grant access to an application" do
-        application = create(:application, name: "app-name")
         sign_in create(:organisation_admin_user)
 
         get :index
 
         assert_select "tr td", text: "app-name"
-        assert_select "form[action='#{account_application_signin_permission_path(application)}']", count: 0
+        assert_select "form[action='#{account_application_signin_permission_path(@application)}']", count: 0
       end
 
       should "not display the button to remove access to an application" do
-        application = create(:application, name: "app-name")
-        application.signin_permission.update!(delegatable: false)
-        user = create(:organisation_admin_user, with_signin_permissions_for: [application])
+        @application.signin_permission.update!(delegatable: false)
+        user = create(:organisation_admin_user, with_signin_permissions_for: [@application])
 
         sign_in user
 
         get :index
 
         assert_select "tr td", text: "app-name"
-        assert_select "a[href='#{delete_account_application_signin_permission_path(application)}']", count: 0
+        assert_select "a[href='#{delete_account_application_signin_permission_path(@application)}']", count: 0
       end
     end
   end
