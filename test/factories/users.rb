b2e677e37794aca15b8a4bc6fdefad0b290b41c1
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-11-08 17:02:55 +0000

    Change default name in User factory to be less confusing
    I've seen the string "A name is now required" on a number of occasions
    when trying to debug a failing test and assumed that it related to some
    kind of unexpected validation error.
    
    This changes the name to use a sequence a bit like the email attribute
    which should be a lot less confusing in similar circumstances.

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     sequence(:email) { |n| "user#{n}@example.com" }
     password { "this 1s 4 v3333ry s3cur3 p4ssw0rd.!Z" }
     confirmed_at { 1.day.ago }
-    name { "A name is now required" }
+    sequence(:name) { |n| "user-name-#{n}" }
     role { "normal" }
 
     after(:create) do |user, evaluator|
