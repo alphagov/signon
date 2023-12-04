0fb10d6268bf67be148030aecfc9aad30df90633
Chris Roos <chris@seagul.co.uk>
2023-12-04 14:14:54 +0000

    Extract Users::SigninPermissionsController#set_user
    To DRY up the controller.
    
    Co-authored-by: Chris Lowis <chris.lowis@gofreerange.com>

--- app/controllers/users/signin_permissions_controller.rb
+++ app/controllers/users/signin_permissions_controller.rb
@@ -2,38 +2,39 @@ class Users::SigninPermissionsController < ApplicationController
   layout "admin_layout"
 
   before_action :authenticate_user!
+  before_action :set_user
 
   def create
-    user = User.find(params[:user_id])
+    authorize UserApplicationPermission.for(@user, application)
 
-    authorize UserApplicationPermission.for(user, application)
+    params = { supported_permission_ids: @user.supported_permissions.map(&:id) + [application.signin_permission.id] }
+    UserUpdate.new(@user, params, current_user, user_ip_address).call
 
-    params = { supported_permission_ids: user.supported_permissions.map(&:id) + [application.signin_permission.id] }
-    UserUpdate.new(user, params, current_user, user_ip_address).call
-
-    redirect_to user_applications_path(user)
+    redirect_to user_applications_path(@user)
   end
 
   def delete
-    @user = User.find(params[:user_id])
     signin_permission = @user.application_permissions.find_by!(supported_permission: application.signin_permission)
 
     authorize signin_permission
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    signin_permission = user.application_permissions.find_by!(supported_permission: application.signin_permission)
+    signin_permission = @user.application_permissions.find_by!(supported_permission: application.signin_permission)
 
     authorize signin_permission
 
-    params = { supported_permission_ids: user.supported_permissions.map(&:id) - [application.signin_permission.id] }
-    UserUpdate.new(user, params, current_user, user_ip_address).call
+    params = { supported_permission_ids: @user.supported_permissions.map(&:id) - [application.signin_permission.id] }
+    UserUpdate.new(@user, params, current_user, user_ip_address).call
 
-    redirect_to user_applications_path(user)
+    redirect_to user_applications_path(@user)
   end
 
 private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 
   def application
     @application ||= Doorkeeper::Application.find(params[:application_id])
