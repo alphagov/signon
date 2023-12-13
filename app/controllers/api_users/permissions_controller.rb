0aebdb11cb8fac93aac472b444e70982cabaf5c6
Chris Lowis <chris.lowis@digital.cabinet-office.gov.uk>
2023-12-13 11:07:27 +0000

    Add ApiUsers::PermissionsController#edit/update
    We've copied the controller, template and test files from their user
    counterparts and modified as necessary. We've decided to add this
    duplication for now but will see whether we can reduce/remove it later.
    
    Note that we're using `UserUpdate` in this new controller, where we were
    using `User#update` in `ApiUsersController#update`[1]. Using
    `UserUpdate` has the following advantages:
    
    - consistency with the other controllers that modify a user's
      permissions (account/permissions_controller and
      users/permissions_controller).
    - record an `EventLog` about the update
    - record an `EventLog` about the permissions that have been
      added/removed
    
    [1]: https://github.com/alphagov/signon/blob/089516afdc0311525769d89c81bc837db4b16a2d/app/controllers/api_users_controller.rb#L42C25-L42C51
    
    Co-authored-by: Chris Roos <chris.roos@gofreerange.com>

---
+++ app/controllers/api_users/permissions_controller.rb
@@ -0,0 +1,49 @@
+class ApiUsers::PermissionsController < ApplicationController
+  layout "admin_layout"
+
+  before_action :authenticate_user!
+  before_action :set_user
+  before_action :set_application
+  before_action :set_permissions
+
+  def edit
+    authorize @api_user
+  end
+
+  def update
+    authorize @api_user
+
+    UserUpdate.new(@api_user, build_user_update_params, current_user, user_ip_address).call
+
+    flash[:application_id] = @application.id
+    redirect_to api_user_applications_path(@api_user)
+  end
+
+private
+
+  def update_params
+    params.require(:application).permit(supported_permission_ids: [])
+  end
+
+  def set_user
+    @api_user = ApiUser.find(params[:api_user_id])
+  end
+
+  def set_application
+    @application = @api_user.authorised_applications.find(params[:application_id])
+  end
+
+  def set_permissions
+    @permissions = @application.sorted_supported_permissions_grantable_from_ui(include_signin: false)
+  end
+
+  def build_user_update_params
+    permissions_user_has = @api_user.supported_permissions.pluck(:id)
+    updatable_permissions_for_this_app = @permissions.pluck(:id)
+    selected_permissions = update_params[:supported_permission_ids].map(&:to_i)
+    permissions_to_add = updatable_permissions_for_this_app.intersection(selected_permissions)
+    permissions_to_remove = updatable_permissions_for_this_app.difference(selected_permissions)
+
+    { supported_permission_ids: (permissions_user_has + permissions_to_add - permissions_to_remove).sort }
+  end
+end
---
+++ test/controllers/api_users/permissions_controller_test.rb
@@ -0,0 +1,347 @@
+require "test_helper"
+
+class ApiUsers::PermissionsControllerTest < ActionController::TestCase
+  context "#edit" do
+    should "prevent unauthenticated users" do
+      application = create(:application)
+      api_user = create(:api_user)
+
+      get :edit, params: { api_user_id: api_user, application_id: application }
+
+      assert_not_authenticated
+    end
+
+    should "prevent unauthorized users" do
+      application = create(:application)
+      api_user = create(:api_user)
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, edit?: false
+
+      get :edit, params: { api_user_id: api_user, application_id: application }
+
+      assert_not_authorised
+    end
+
+    should "display checkboxes for the grantable permissions" do
+      application = create(:application)
+      perm1 = create(:supported_permission, application:, name: "perm-1")
+      perm2 = create(:supported_permission, application:, name: "perm-2")
+      perm3 = create(:supported_permission, application:, name: "perm-3", grantable_from_ui: false)
+      api_user = create(:api_user, with_permissions: { application => %w[perm-1] })
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy_for_navigation_links(current_user)
+      stub_policy current_user, api_user, edit?: true
+
+      get :edit, params: { api_user_id: api_user, application_id: application }
+
+      assert_select "input[type='checkbox'][checked='checked'][name='application[supported_permission_ids][]'][value='#{perm1.id}']"
+      assert_select "input[type='checkbox'][name='application[supported_permission_ids][]'][value='#{perm2.id}']"
+      assert_select "input[type='checkbox'][name='application[supported_permission_ids][]'][value='#{perm3.id}']", count: 0
+      assert_select "input[type='checkbox'][name='application[supported_permission_ids][]'][value='#{application.signin_permission.id}']", count: 0
+    end
+
+    should "include a hidden field for the signin permission so that it is not removed" do
+      application = create(:application, with_supported_permissions: ["perm-1", SupportedPermission::SIGNIN_NAME])
+      api_user = create(:api_user, with_permissions: { application => %w[perm-1] })
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy_for_navigation_links(current_user)
+      stub_policy current_user, api_user, edit?: true
+
+      get :edit, params: { api_user_id: api_user, application_id: application }
+
+      assert_select "input[type='hidden'][value='#{application.signin_permission.id}']"
+    end
+
+    should "exclude retired applications" do
+      sign_in create(:superadmin_user)
+
+      application = create(:application, retired: true)
+      api_user = create(:api_user)
+
+      assert_raises(ActiveRecord::RecordNotFound) do
+        get :edit, params: { api_user_id: api_user, application_id: application }
+      end
+    end
+
+    should "allow editing permissions for API-only application" do
+      sign_in create(:superadmin_user)
+
+      application = create(:application, api_only: true)
+      api_user = create(:api_user)
+      create(:access_token, resource_owner_id: api_user.id, application:)
+
+      get :edit, params: { api_user_id: api_user, application_id: application }
+
+      assert_response :success
+    end
+
+    should "raise an exception if the user does not have an access token for the application" do
+      application = create(:application)
+      api_user = create(:api_user)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      assert_raises(ActiveRecord::RecordNotFound) do
+        get :edit, params: { api_user_id: api_user, application_id: application }
+      end
+    end
+  end
+
+  context "#update" do
+    should "prevent unauthenticated users" do
+      application = create(:application)
+      api_user = create(:api_user)
+
+      patch :update, params: { api_user_id: api_user, application_id: application }
+
+      assert_not_authenticated
+    end
+
+    should "replace the users permissions with new ones" do
+      application = create(:application, with_supported_permissions: %w[new old])
+      api_user = create(:api_user, with_permissions: { application => %w[old] })
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: true
+
+      new_permission = application.supported_permissions.find_by(name: "new")
+
+      expected_params = { supported_permission_ids: [new_permission.id] }
+      user_update = stub("user-update").responds_like_instance_of(UserUpdate)
+      user_update.expects(:call)
+      UserUpdate.stubs(:new).with(api_user, expected_params, current_user, anything).returns(user_update)
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [new_permission.id] } }
+    end
+
+    should "redirect once the permissions have been updated" do
+      application = create(:application, with_supported_permissions: %w[new old])
+      api_user = create(:api_user, with_permissions: { application => %w[old] })
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: true
+
+      new_permission = application.supported_permissions.find_by(name: "new")
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [new_permission.id] } }
+
+      assert_redirected_to api_user_applications_path(api_user)
+    end
+
+    should "retain permissions for other apps" do
+      other_application = create(:application, with_supported_permissions: %w[other])
+      application = create(:application, with_supported_permissions: %w[new old])
+      api_user = create(:api_user, with_permissions: { application => %w[old], other_application => %w[other] })
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: true
+
+      new_permission = application.supported_permissions.find_by(name: "new")
+      other_permission = other_application.supported_permissions.find_by(name: "other")
+
+      expected_params = { supported_permission_ids: [other_permission.id, new_permission.id].sort }
+      user_update = stub("user-update").responds_like_instance_of(UserUpdate)
+      user_update.expects(:call)
+      UserUpdate.stubs(:new).with(api_user, expected_params, current_user, anything).returns(user_update)
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [new_permission.id] } }
+    end
+
+    should "prevent permissions being added for apps that the current user does not have access to" do
+      organisation = create(:organisation)
+
+      application1 = create(:application)
+      application2 = create(:application, with_supported_permissions: %w[app2-permission])
+
+      api_user = create(:api_user, organisation:)
+      create(:access_token, application: application1, resource_owner_id: api_user.id)
+      create(:access_token, application: application2, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      current_user.grant_application_signin_permission(application1)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: true
+
+      app2_permission = application2.supported_permissions.find_by!(name: "app2-permission")
+
+      patch :update, params: { api_user_id: api_user, application_id: application1, application: { supported_permission_ids: [app2_permission.id] } }
+
+      api_user.reload
+
+      assert_equal [], api_user.supported_permissions
+    end
+
+    should "not remove the signin permission from the app when updating other permissions" do
+      application = create(:application, with_supported_permissions: %w[other])
+      api_user = create(:api_user)
+      api_user.grant_application_signin_permission(application)
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: true
+
+      other_permission = application.supported_permissions.find_by(name: "other")
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [other_permission.id] } }
+
+      api_user.reload
+      assert_same_elements [application.signin_permission, other_permission], api_user.supported_permissions
+    end
+
+    should "not remove permissions the user already has that are not grantable from ui" do
+      application = create(:application, with_supported_permissions: %w[other], with_supported_permissions_not_grantable_from_ui: %w[not_from_ui])
+      api_user = create(:api_user)
+      api_user.grant_application_permission(application, "not_from_ui")
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: true
+
+      other_permission = application.supported_permissions.find_by(name: "other")
+      not_from_ui_permission = application.supported_permissions.find_by(name: "not_from_ui")
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [other_permission.id] } }
+
+      api_user.reload
+
+      assert_same_elements [other_permission, not_from_ui_permission], api_user.supported_permissions
+    end
+
+    should "prevent permissions being added for other apps" do
+      other_application = create(:application, with_supported_permissions: %w[other])
+      application = create(:application)
+      api_user = create(:api_user)
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: true
+
+      other_permission = other_application.supported_permissions.find_by(name: "other")
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [other_permission.id] } }
+
+      api_user.reload
+
+      assert_equal [], api_user.supported_permissions
+    end
+
+    should "prevent permissions being added that are not grantable from the ui" do
+      application = create(:application, with_supported_permissions: %w[other], with_supported_permissions_not_grantable_from_ui: %w[not_from_ui])
+      api_user = create(:api_user)
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: true
+
+      not_from_ui_permission = application.supported_permissions.find_by(name: "not_from_ui")
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [not_from_ui_permission.id] } }
+
+      api_user.reload
+
+      assert_equal [], api_user.supported_permissions
+    end
+
+    should "assign the application id to the application_id flash" do
+      application = create(:application, with_supported_permissions: %w[permission])
+      api_user = create(:api_user)
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: true
+
+      permission = application.supported_permissions.find_by(name: "permission")
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [permission.id] } }
+
+      assert_equal application.id, flash[:application_id]
+    end
+
+    should "raise an exception if the user cannot be found" do
+      application = create(:application)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      assert_raises(ActiveRecord::RecordNotFound) do
+        patch :update, params: { api_user_id: "unknown-id", application_id: application, application: { supported_permission_ids: %w[id] } }
+      end
+    end
+
+    should "prevent unauthorised users" do
+      application = create(:application)
+      api_user = create(:api_user)
+      create(:access_token, application:, resource_owner_id: api_user.id)
+
+      current_user = create(:superadmin_user)
+      sign_in current_user
+
+      stub_policy current_user, api_user, update?: false
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [] } }
+
+      assert_not_authorised
+    end
+
+    should "allow editing permissions for API-only application" do
+      sign_in create(:superadmin_user)
+
+      application = create(:application, api_only: true, with_supported_permissions: %w[permission])
+      api_user = create(:api_user)
+      create(:access_token, resource_owner_id: api_user.id, application:)
+
+      permission = application.supported_permissions.find_by(name: "permission")
+
+      assert_nothing_raised do
+        patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [permission.id] } }
+      end
+    end
+
+    should "push permission changes out to apps" do
+      sign_in create(:superadmin_user)
+
+      application = create(:application, with_supported_permissions: %w[permission])
+      api_user = create(:api_user)
+      create(:access_token, resource_owner_id: api_user.id, application:)
+
+      permission = application.supported_permissions.find_by(name: "permission")
+
+      PermissionUpdater.expects(:perform_on).with(api_user).once
+
+      patch :update, params: { api_user_id: api_user, application_id: application, application: { supported_permission_ids: [permission.id] } }
+    end
+  end
+end
