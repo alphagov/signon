edf2e83e56b120d25a7ce7853a268f5383ed2654
Chris Roos <chris@seagul.co.uk>
2023-12-14 12:27:25 +0000

    Fix Account::PermissionsController#update
    By duplicating the tests and implementation we've recently added for
    `Users::PermissionsController#update`.
    
    We discussed extracting a method to remove the duplication between the
    two `build_user_update_params` methods but couldn't agree on where the
    responsibility should live, so we've decided to leave it for now.
    
    Note the change to `PunditHelpers#stub_policy` where we've updated it to
    handle namespaced policies.
    
    Co-authored-by: Chris Lowis <chris.lowis@gofreerange.com>

--- test/support/pundit_helpers.rb
+++ test/support/pundit_helpers.rb
@@ -1,6 +1,7 @@
 module PunditHelpers
   def stub_policy(current_user, record, method_and_return_value)
     policy_class = Pundit::PolicyFinder.new(record).policy
+    record = record.last if record.is_a?(Array)
     policy = stub_everything("policy", method_and_return_value).responds_like_instance_of(policy_class)
     policy_class.stubs(:new).with(current_user, record).returns(policy)
   end
