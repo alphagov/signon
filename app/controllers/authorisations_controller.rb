222dab8d100f436be7e34c4c84d89d41a95c4db1
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-12-14 12:22:27 +0000

    Use route helper methods in API user access token pages
    I think this makes the code more readable.

--- app/controllers/authorisations_controller.rb
+++ app/controllers/authorisations_controller.rb
@@ -23,7 +23,7 @@ class AuthorisationsController < ApplicationController
     else
       flash[:error] = "There was an error while creating the access token"
     end
-    redirect_to [:manage_tokens, @api_user]
+    redirect_to manage_tokens_api_user_path(@api_user)
   end
 
   def revoke
@@ -34,7 +34,7 @@ class AuthorisationsController < ApplicationController
     else
       flash[:error] = "There was an error while revoking access for #{authorisation.application.name}"
     end
-    redirect_to [:manage_tokens, @api_user]
+    redirect_to manage_tokens_api_user_path(@api_user)
   end
 
 private
