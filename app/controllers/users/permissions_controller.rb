4ecb63b081ceee42d421127eef41988ce40022fb
Chris Roos <chris@seagul.co.uk>
2023-12-14 11:52:24 +0000

    Extract Users::PermissionsController#set_permissions
    Co-authored-by: Chris Lowis <chris.lowis@gofreerange.com>

--- app/controllers/users/permissions_controller.rb
+++ app/controllers/users/permissions_controller.rb
@@ -4,6 +4,7 @@ class Users::PermissionsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
   before_action :set_application
+  before_action :set_permissions, only: %i[edit update]
 
   def show
     authorize @user, :edit?
@@ -15,15 +16,13 @@ class Users::PermissionsController < ApplicationController
 
   def edit
     authorize UserApplicationPermission.for(@user, @application)
-
-    @permissions = @application.sorted_supported_permissions_grantable_from_ui(include_signin: false)
   end
 
   def update
     authorize UserApplicationPermission.for(@user, @application)
 
     permissions_user_has = @user.supported_permissions.pluck(:id)
-    updatable_permissions_for_this_app = @application.sorted_supported_permissions_grantable_from_ui(include_signin: false).pluck(:id)
+    updatable_permissions_for_this_app = @permissions.pluck(:id)
     selected_permissions = update_params[:supported_permission_ids].map(&:to_i)
     permissions_to_add = updatable_permissions_for_this_app.intersection(selected_permissions)
     permissions_to_remove = updatable_permissions_for_this_app.difference(selected_permissions)
@@ -47,5 +46,9 @@ private
 
   def set_application
     @application = Doorkeeper::Application.with_signin_permission_for(@user).not_api_only.find(params[:application_id])
+  end
+
+  def set_permissions
+    @permissions = @application.sorted_supported_permissions_grantable_from_ui(include_signin: false)
   end
 end
