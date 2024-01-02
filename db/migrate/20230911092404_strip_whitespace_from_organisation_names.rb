5efbf650c51fc0f89f3a04292b1432101a5a0bbb
James Mead <james.mead@digital.cabinet-office.gov.uk>
2024-01-02 12:33:10 +0000

    Auto-correct Rails/FindEach rubocop violations
    > Use find_each instead of each. (https://rails.rubystyle.guide#find-each)

--- db/migrate/20230911092404_strip_whitespace_from_organisation_names.rb
+++ db/migrate/20230911092404_strip_whitespace_from_organisation_names.rb
@@ -2,7 +2,7 @@ class StripWhitespaceFromOrganisationNames < ActiveRecord::Migration[7.0]
   NAME_HAS_LEADING_OR_TRAILING_SPACE = "name REGEXP('^\s+') OR name REGEXP('\s+$')".freeze
 
   def up
-    Organisation.where(NAME_HAS_LEADING_OR_TRAILING_SPACE).each(&:save!)
+    Organisation.where(NAME_HAS_LEADING_OR_TRAILING_SPACE).find_each(&:save!)
   end
 
   def down; end
--- db/migrate/20230925104849_strip_whitespace_from_organisation_name.rb
+++ db/migrate/20230925104849_strip_whitespace_from_organisation_name.rb
@@ -1,6 +1,6 @@
 class StripWhitespaceFromOrganisationName < ActiveRecord::Migration[7.0]
   def change
-    Organisation.where("name REGEXP ? OR name REGEXP ?", "^\\s+", "\\s+$").each do |o|
+    Organisation.where("name REGEXP ? OR name REGEXP ?", "^\\s+", "\\s+$").find_each do |o|
       o.update_attribute(:name, o.name&.strip) # rubocop:disable Rails/SkipsModelValidations
     end
   end
