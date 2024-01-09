dee52416ba4a9c7b3ec3e4f11a223f5823927369
Chris Lowis <chris.lowis@digital.cabinet-office.gov.uk>
2024-01-09 12:18:57 +0000

    Remove layouts/application.html.erb
    This layout is no longer used by the application so can be
    removed. We've also removed the dependent legacy stylesheets, javascript and
    the vendored `chosen` code as well as the shared
    bootstrap_flash_messages partial and its helper since these are no
    longer used elsewhere in the app.
    
    Note that since 5b0f80d4 we are implementing the
    same sanitization of tokens in URLs that was being performed in this
    layout by the `sensitive_query_parameters?` block.
    
    Co-authored-by: Chris Roos <chris.roos@gofreerange.com>

--- app/assets/javascripts/legacy/modules/chosen.js
+++
@@ -1,9 +0,0 @@
-(function (Modules) {
-  'use strict'
-  Modules.Chosen = function () {
-    var that = this
-    that.start = function (element) {
-      element.chosen()
-    }
-  }
-})(window.GOVUKAdmin.Modules)
--- app/assets/javascripts/legacy_layout.js
+++
@@ -1,2 +0,0 @@
-//= require chosen.jquery
-//= require_directory ./legacy/modules
