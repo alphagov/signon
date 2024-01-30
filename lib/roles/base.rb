c9ead4b7efca253ab9037d28ce18e1cd6a817e09
Chris Roos <chris@seagul.co.uk>
2024-01-30 17:08:08 +0000

    Sort `Roles::ClassMethods.role_classes` by level
    In order to remove some duplication.

--- lib/roles/base.rb
+++ lib/roles/base.rb
@@ -1,7 +1,7 @@
 module Roles
   class Base
     def self.manageable_roles
-      User.role_classes.select { |role_class| role_class.level >= level }.sort_by(&:level).reverse.map(&:role_name)
+      User.role_classes.select { |role_class| role_class.level >= level }.reverse.map(&:role_name)
     end
   end
 end
