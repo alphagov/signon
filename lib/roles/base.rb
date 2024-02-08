0ed9031d3ad913c0001d4ddeddfcec7e16ff2c44
Chris Roos <chris@seagul.co.uk>
2024-02-08 12:00:57 +0000

    Extract `Roles::<class>.can_manage?`
    It feels as though the individual role classes are best placed to know
    whether they can manage another role.

--- lib/roles/base.rb
+++ lib/roles/base.rb
@@ -3,5 +3,9 @@ module Roles
     def self.manageable_roles
       Roles.all.select { |role_class| role_class.level >= level }.reverse.map(&:role_name)
     end
+
+    def self.can_manage?(role_name)
+      manageable_roles.include?(role_name)
+    end
   end
 end
