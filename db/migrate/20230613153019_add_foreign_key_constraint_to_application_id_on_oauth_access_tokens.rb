a6ef0daf0a33c062caf16cd41a201fb87226eab5
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-02-26 16:39:07 +0000

    Remove migrations from before 2024
    This removes legacy migrations that have already been run in production.
    
    Keeping around legacy migrations makes it difficult to identify which
    code needs to be changed when making large scale changes and clutters
    the results of searches.
    
    Prior art for this occurred in 3797cd922538a9a8564a775ba53b4e18689b4f9a.

--- db/migrate/20230613153019_add_foreign_key_constraint_to_application_id_on_oauth_access_tokens.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyConstraintToApplicationIdOnOauthAccessTokens < ActiveRecord::Migration[7.0]
-  def change
-    add_foreign_key :oauth_access_tokens, :oauth_applications, column: :application_id
-  end
-end
--- db/migrate/20230816164936_revoke_access_tokens_for_suspended_users.rb
+++
@@ -1,8 +0,0 @@
-class RevokeAccessTokensForSuspendedUsers < ActiveRecord::Migration[7.0]
-  def up
-    User.with_status(User::USER_STATUS_SUSPENDED)
-      .find_each(&:revoke_all_authorisations)
-  end
-
-  def down; end
-end
