dba8b6f5c74c42d64d8ca994f1270770fb939f89
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-02-26 12:30:40 +0000

    Use Rails 7.1 defaults
    All deprecations and breaking changes have now been resolved, so we can
    switch to Rails 7.1 defaults.

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module Signon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     config.active_record.belongs_to_required_by_default = false
 
