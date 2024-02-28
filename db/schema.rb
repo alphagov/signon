24467c7aa6418378a9afeb70d211c0a66f4bb4ec
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-02-28 08:01:58 +0000

    Re-encrypt all users
    In Rails 7.0, the encryption algorithm was changed from SHA-1 to SHA-256
    [1] but users who generated their OTP key when we used an older version
    of Rails  would still have their key stored with SHA-1. Rails 7.0 could
    support both types of encryption by default, so we saw no issues.
    
    However in Rails 7.1, the option
    `support_sha1_for_non_deterministic_encryption` is set to `false` by
    default, meaning users encrypted with SHA-1 would be unable to login.
    
    We therefore changed this setting to `true` when upgrading to Rails 7.1
    [2]. By re-encrypting all users with SHA-256, we can disable this
    setting.
    
    1: https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#active-record-encryption-algorithm-changes
    2: https://github.com/alphagov/signon/pull/2745/commits/90d4cf0ea3f0df5013b7fedfeddcd0570b90cc10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_080657) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_080000) do
   create_table "batch_invitation_application_permissions", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.integer "batch_invitation_id", null: false
     t.integer "supported_permission_id", null: false
