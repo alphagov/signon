63e74ad59e7836eb4ebaaebb939d05daa4f423c2
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2024-06-13 16:17:09 +0100

    Hide account activity events from non-admins
    Visibility of these events was causing confusion for some users, so restricting
    visibility to admin/superadmin to avoid this.

--- test/integration/account_activities_test.rb
+++ test/integration/account_activities_test.rb
@@ -12,5 +12,30 @@ class AccountActivitiesTest < ActionDispatch::IntegrationTest
 
       assert page.has_selector? "td", text: "Successful login"
     end
+    should "not show technical events to normal users" do
+      user = create(:user)
+      EventLog.record_event(user, EventLog::ACCESS_GRANTS_DELETED)
+
+      visit new_user_session_path
+      signin_with user
+
+      visit account_activity_path
+
+      assert page.has_selector? "td", text: "Successful login"
+      assert_no_text "Access grants deleted"
+    end
+
+    should "show technical events to admin/superadmin users" do
+      user = create(:admin_user)
+      EventLog.record_event(user, EventLog::ACCESS_GRANTS_DELETED)
+
+      visit new_user_session_path
+      signin_with user
+
+      visit account_activity_path
+
+      assert page.has_selector? "td", text: "Successful login"
+      assert page.has_selector? "td", text: "Access grants deleted"
+    end
   end
 end
