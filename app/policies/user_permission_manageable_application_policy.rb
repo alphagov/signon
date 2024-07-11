597b835f245ccdc35397cd6e4ea9f01a43635beb
Ynda Jas <yndajas@gmail.com>
2024-07-11 17:09:43 +0100

    WIP investigate `UserPermissionManageableApplicationPolicy`

--- app/policies/user_permission_manageable_application_policy.rb
+++ app/policies/user_permission_manageable_application_policy.rb
@@ -22,6 +22,14 @@ class UserPermissionManageableApplicationPolicy
       if current_user.govuk_admin?
         applications
       elsif current_user.publishing_manager?
+        # this policy is used in a few places - I think this might need further research
+        # to determine if it's being used correctly and if anything needs changing.
+        # On initial inspection, it looks like another area where the signin
+        # permission is too powerful/impactful and I'd expect
+        # `applications.can_signin(current_user)` to be more accurate here, with
+        # permissions-related filters applied elsewhere, e.g. the SupportedPermissionPolicy
+        # The change in the SupportedPermissionPolicy to filter by delegatable
+        # might be enough for our purposes though?
         applications.can_signin(current_user).with_signin_delegatable
       else
         applications.none
