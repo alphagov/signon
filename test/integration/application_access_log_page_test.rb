0e9d467e6b11d8c170acf342663b833bdad5de74
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-08-09 16:23:02 +0100

    Add integration test for application access logs

---
+++ test/integration/application_access_log_page_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+class ApplicationAccessLogPageIntegrationTest < ActionDispatch::IntegrationTest
+  setup do
+    @application = create(:application, name: "app-name", description: "app-description")
+    @user = create(:user, name: "Normal User")
+  end
+
+  test "users don't have permission to view account access log" do
+    visit root_path
+    signin_with(@user)
+
+    visit access_logs_doorkeeper_application_path(@application)
+    flash = find("div[role='alert']")
+    assert flash.has_content?("You do not have permission to perform this action.")
+  end
+
+  context "logged in as an superadmin" do
+    setup do
+      visit new_user_session_path
+      @superadmin = create(:superadmin_user)
+      signin_with(@superadmin)
+    end
+
+    should "have permission to view account access log" do
+      visit access_logs_doorkeeper_application_path(@application)
+      assert_equal page.title, "app-name access log - GOV.UK Signon"
+    end
+
+    context "when there are no matching events" do
+      should "see a message stating that there is no activity logged" do
+        visit access_logs_doorkeeper_application_path(@application)
+        assert_text "app-name access log"
+        assert_text "No activity logged"
+      end
+    end
+
+    context "when there are matching events" do
+      setup do
+        create(:event_log, event_id: 47, application_id: @application.id, uid: @superadmin.uid)
+        create(:event_log, event_id: 47, application_id: @application.id, uid: @user.uid)
+      end
+
+      should "see a list of events for the application" do
+        visit access_logs_doorkeeper_application_path(@application)
+        assert_text "#{@application.name} access log"
+        assert_text "Successful user application authorization for #{@application.name} for #{@superadmin.name}"
+        assert_text "Successful user application authorization for #{@application.name} for #{@user.name}"
+      end
+    end
+  end
+
+end
