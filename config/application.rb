19f602667735a55fdfabc0b22565b4f21919669d
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-08-15 14:45:31 +0100

    Run `rails app:update`
    This updates the application settings for Rails 7.2, which was upgraded
    in https://github.com/alphagov/signon/pull/3084.

--- config/application.rb
+++ config/application.rb
@@ -90,6 +90,7 @@ module Signon
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.show_user_research_recruitment_banner = false
