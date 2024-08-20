95903e1318e6aa621aab343c7cbc43ed8f0bd04c
George Eaton <george@dxw.com>
2024-08-20 09:18:28 +0100

    Combine "happy path" tests
    There felt like no obvious happy path in these tests at a quick glance,
    which made these tests a bit hard to read.
    
    This commit also improves variable naming and reduces reliance on
    strings in test assertions to hopefully make things a bit easier to
    read.

--- test/controllers/api_users/permissions_controller_test.rb
+++ test/controllers/api_users/permissions_controller_test.rb
@@ -26,12 +26,13 @@ class ApiUsers::PermissionsControllerTest < ActionController::TestCase
       assert_not_authorised
     end
 
-    should "display checkboxes for the grantable permissions" do
+    should "render a page with checkboxes for the grantable permissions and a hidden field for the signin permission so that it is not removed" do
       application = create(:application)
-      perm1 = create(:supported_permission, application:, name: "perm-1")
-      perm2 = create(:supported_permission, application:, name: "perm-2")
-      perm3 = create(:supported_permission, application:, name: "perm-3", grantable_from_ui: false)
-      api_user = create(:api_user, with_permissions: { application => %w[perm-1] })
+      old_grantable_permission = create(:supported_permission, application:)
+      new_grantable_permission = create(:supported_permission, application:)
+      new_non_grantable_permission = create(:supported_permission, application:, grantable_from_ui: false)
+
+      api_user = create(:api_user, with_permissions: { application => [old_grantable_permission.name] })
       create(:access_token, application:, resource_owner_id: api_user.id)
 
       current_user = create(:superadmin_user)
@@ -42,25 +43,10 @@ class ApiUsers::PermissionsControllerTest < ActionController::TestCase
 
       get :edit, params: { api_user_id: api_user, application_id: application }
 
-      assert_select "input[type='checkbox'][checked='checked'][name='application[supported_permission_ids][]'][value='#{perm1.id}']"
-      assert_select "input[type='checkbox'][name='application[supported_permission_ids][]'][value='#{perm2.id}']"
-      assert_select "input[type='checkbox'][name='application[supported_permission_ids][]'][value='#{perm3.id}']", count: 0
+      assert_select "input[type='checkbox'][checked='checked'][name='application[supported_permission_ids][]'][value='#{old_grantable_permission.id}']"
+      assert_select "input[type='checkbox'][name='application[supported_permission_ids][]'][value='#{new_grantable_permission.id}']"
+      assert_select "input[type='checkbox'][name='application[supported_permission_ids][]'][value='#{new_non_grantable_permission.id}']", count: 0
       assert_select "input[type='checkbox'][name='application[supported_permission_ids][]'][value='#{application.signin_permission.id}']", count: 0
-    end
-
-    should "include a hidden field for the signin permission so that it is not removed" do
-      application = create(:application, with_non_delegatable_supported_permissions: %w[perm-1])
-      api_user = create(:api_user, with_permissions: { application => %w[perm-1] })
-      create(:access_token, application:, resource_owner_id: api_user.id)
-
-      current_user = create(:superadmin_user)
-      sign_in current_user
-
-      stub_policy_for_navigation_links(current_user)
-      stub_policy current_user, api_user, edit?: true
-
-      get :edit, params: { api_user_id: api_user, application_id: application }
-
       assert_select "input[type='hidden'][value='#{application.signin_permission.id}']"
     end
 
