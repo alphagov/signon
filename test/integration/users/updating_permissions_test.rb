51361b60e32132024eabcb81b32108b37a220e71
Ynda Jas <yndajas@gmail.com>
2024-09-08 14:42:42 +0100

    Move users updating <= 8 perm's integration tests
    ... to new structure, which improves consistency between account and
    users namespaces. The tests for when there are more than eight
    permissions will be moved over later

--- test/integration/users/updating_permissions_test.rb
+++ test/integration/users/updating_permissions_test.rb
@@ -4,73 +4,194 @@ class Users::UpdatingPermissionsTest < ActionDispatch::IntegrationTest
   # See also: UpdatingPermissionsForAppsWithManyPermissionsTest
 
   context "for all apps" do
+    setup do
+      @application = create(:application)
+
+      @old_delegatable_grantable_permission,
+      @new_delegatable_grantable_permission = Array.new(2).map do |_|
+        create(:delegatable_supported_permission, application: @application)
+      end
+
+      @old_non_delegatable_grantable_permission,
+      @new_non_delegatable_grantable_permission = Array.new(2).map do |_|
+        create(:non_delegatable_supported_permission, application: @application)
+      end
+
+      @old_delegatable_non_grantable_permission,
+      @new_delegatable_non_grantable_permission = Array.new(2).map do |_|
+        create(
+          :delegatable_supported_permission,
+          application: @application,
+          grantable_from_ui: false,
+        )
+      end
+
+      @granter = create(:user_in_organisation, with_signin_permissions_for: [@application])
+      @grantee = create(
+        :user,
+        organisation: @granter.organisation,
+        with_signin_permissions_for: [@application],
+        with_permissions: { @application => [
+          @old_delegatable_grantable_permission,
+          @old_non_delegatable_grantable_permission,
+          @old_delegatable_non_grantable_permission,
+        ].map(&:name) },
+      )
+    end
+
     context "when the grantee is in the same organisation, and the granter has access" do
       %w[superadmin admin super_organisation_admin organisation_admin].each do |role|
         context "as a #{role}" do
+          setup do
+            @granter.update!(role:)
+            visit new_user_session_path
+            signin_with @granter
+          end
+
           should "be able to grant delegatable non-signin permissions that are grantable from the UI" do
-            skip
+            assert_update_permissions_for_other_user(
+              @application, @grantee,
+              grant: [@new_delegatable_grantable_permission],
+              revoke: [@old_delegatable_grantable_permission]
+            )
+
+            refute_update_permissions_for_other_user(
+              @application,
+              [@old_delegatable_non_grantable_permission, @new_delegatable_non_grantable_permission],
+              @grantee,
+            )
           end
         end
       end
 
       %w[superadmin admin].each do |admin_role|
         context "as a #{admin_role}" do
+          setup do
+            @granter.update!(role: admin_role)
+            visit new_user_session_path
+            signin_with @granter
+          end
+
           should "be able to grant non-delegatable permissions" do
-            skip
+            assert_update_permissions_for_other_user(
+              @application, @grantee,
+              grant: [@new_non_delegatable_grantable_permission],
+              revoke: [@old_non_delegatable_grantable_permission]
+            )
           end
         end
       end
 
       %w[super_organisation_admin organisation_admin].each do |publishing_manager_role|
         context "as a #{publishing_manager_role}" do
+          setup do
+            @granter.update!(role: publishing_manager_role)
+            visit new_user_session_path
+            signin_with @granter
+          end
+
           should "not be able to grant non-delegatable permissions" do
-            skip
+            refute_update_permissions_for_other_user(
+              @application,
+              [@new_non_delegatable_grantable_permission, @old_non_delegatable_grantable_permission],
+              @grantee,
+            )
           end
         end
       end
     end
 
     context "when the grantee is not in the same organisation" do
+      setup { @grantee.update!(organisation: create(:organisation)) }
+
       %w[superadmin admin].each do |admin_role|
         context "as a #{admin_role}" do
+          setup do
+            @granter.update!(role: admin_role)
+            visit new_user_session_path
+            signin_with @granter
+          end
+
           should "be able to grant permissions" do
-            skip
+            assert_update_permissions_for_other_user(
+              @application, @grantee,
+              grant: [@new_delegatable_grantable_permission],
+              revoke: [@old_delegatable_grantable_permission]
+            )
           end
         end
       end
 
       context "as a super_organisation_admin" do
-        should("not be able to edit the user") { skip }
+        setup do
+          @granter.update!(role: "super_organisation_admin")
+          visit new_user_session_path
+          signin_with @granter
+        end
+
+        should("not be able to edit the user") { refute_edit_other_user(@grantee) }
 
         context "but the grantee's organisation is a child of the granter's" do
+          setup { @grantee.update!(organisation: create(:organisation, parent: @granter.organisation)) }
+
           should "be able to grant permissions" do
-            skip
+            assert_update_permissions_for_other_user(
+              @application, @grantee,
+              grant: [@new_delegatable_grantable_permission],
+              revoke: [@old_delegatable_grantable_permission]
+            )
           end
         end
       end
 
-      context "as a super_organisation_admin" do
-        should("not be able to edit the user") { skip }
+      context "as a organisation_admin" do
+        setup do
+          @granter.update!(role: "organisation_admin")
+          visit new_user_session_path
+          signin_with @granter
+        end
+
+        should("not be able to edit the user") { refute_edit_other_user(@grantee) }
 
         context "but the grantee's organisation is a child of the granter's" do
-          should("not be able to edit the user") { skip }
+          setup { @grantee.update!(organisation: create(:organisation, parent: @granter.organisation)) }
+
+          should("not be able to edit the user") { refute_edit_other_user(@grantee) }
         end
       end
     end
 
-    context "when the granter does not have permissions" do
+    context "when the granter does not have access" do
+      setup { UserApplicationPermission.find_by(user: @granter, supported_permission: @application.signin_permission).destroy }
+
       %w[superadmin admin].each do |admin_role|
         context "as a #{admin_role}" do
+          setup do
+            @granter.update!(role: admin_role)
+            visit new_user_session_path
+            signin_with @granter
+          end
+
           should "be able to grant permissions" do
-            skip
+            assert_update_permissions_for_other_user(
+              @application, @grantee,
+              grant: [@new_delegatable_grantable_permission],
+              revoke: [@old_delegatable_grantable_permission]
+            )
           end
         end
       end
 
       %w[super_organisation_admin organisation_admin].each do |publishing_manager_role|
         context "as a #{publishing_manager_role}" do
+          setup do
+            @granter.update!(role: publishing_manager_role)
+            visit new_user_session_path
+            signin_with @granter
+          end
+
           should "not be able to grant any permissions for the app" do
-            skip
+            refute_update_any_permissions_for_app_for_other_user(@application, @grantee)
           end
         end
       end
