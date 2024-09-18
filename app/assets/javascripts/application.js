775e509b9fe1ed43e613afcf788613bae11b9e1f
Ynda Jas <yndajas@gmail.com>
2024-09-18 15:01:39 +0100

    Use password input component
    There's now a password input component in govuk_publishing_components,
    implementing the password input from GOV.UK Design System v5. This adds
    a show/hide password button when JavaScript is enabled, which has
    previously been requested by a Signon user for accessibility purposes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/components/copy-to-clipboard
 //= require govuk_publishing_components/components/option-select
+//= require govuk_publishing_components/components/password-input
 //= require govuk_publishing_components/components/table
 
 //= require_tree ./modules
