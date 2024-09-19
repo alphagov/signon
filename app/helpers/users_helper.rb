61212506e06a1070cb5e7268b0b3941e4eeb4fa0
Ynda Jas <yndajas@gmail.com>
2024-09-19 16:47:14 +0100

    Remove unused methods
    These aren't used anywhere and have no tests

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -27,20 +27,6 @@ module UsersHelper
     end
   end
 
-  def organisation_options(form_builder)
-    accessible_organisations = policy_scope(Organisation)
-    options_from_collection_for_select(
-      accessible_organisations,
-      :id,
-      :name_with_abbreviation,
-      selected: form_builder.object.organisation_id,
-    )
-  end
-
-  def organisation_select_options
-    { include_blank: current_user.publishing_manager? ? false : Organisation::NONE }
-  end
-
   def user_email_tokens(user = current_user)
     [user.email] + DeviseZxcvbn::EmailTokeniser.split(user.email)
   end
