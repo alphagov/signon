fb6d9fa097e1f3374d6df84b93341199e9a81b7e
Ynda Jas <yndajas@gmail.com>
2024-10-03 17:37:57 +0100

    Fix delegated column rename migration
    Looks like a mistake creeped in and we missed it. The migration failed
    on Argo, so amending the existing migration should work since it's not
    yet been applied

--- db/migrate/20241001140623_rename_supported_permission_delegatable_column.rb
+++ db/migrate/20241001140623_rename_supported_permission_delegatable_column.rb
@@ -1,5 +1,5 @@
 class RenameSupportedPermissionDelegatableColumn < ActiveRecord::Migration[7.2]
   def change
-    rename_column :supported_permissions, :delegated, :delegated
+    rename_column :supported_permissions, :delegatable, :delegated
   end
 end
