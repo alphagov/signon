34ef4a38df2a0f7d14b61f8c7a06b5f634e90690
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-04-07 09:55:46 +0100

    Revert "Use old Chrome in CI tests, with --no-sandbox flag"
    This reverts commit 0f746838afd34a7b08be9a886d149baca3eb7028.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,17 +14,6 @@ require "mocha/minitest"
 
 GovukTest.configure
 
-Capybara.register_driver :headless_chrome do |app|
-  chrome_options = GovukTest.headless_chrome_selenium_options
-  chrome_options.add_argument("--no-sandbox")
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    options: chrome_options,
-  )
-end
-
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
 
