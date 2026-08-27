726a4b20f9479faba08a5e53559360d4b5c8d6e6
Lucas Mbiwe <lucas.mbiwe@dsit.gov.uk>
2026-08-27 15:19:49 +0100

    Require all ES6 components through the right file
    After a thorough review of govuk_publishing_components and the ES6
    govuk-frontend components it uses, I realised there were three more ES6
    components pulled into the wrong file. This change fixes this.
    
    For anybody interested, this is how I found all of govuk-frontend's ES6
    components used in govuk_publishing_components:
    
    `grep -r 'require govuk/components' app | sort | cut -d / -f 6`
    
    At the time of me writing this, there are 11 such components:
    
    accordion, button, character-count, checkboxes, error-summary,
    file-upload, password-input, radio, service-navigation, skip-link, tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,8 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/copy-to-clipboard
-//= require govuk_publishing_components/components/file-upload
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/option-select
-//= require govuk_publishing_components/components/password-input
-//= require govuk_publishing_components/components/service-navigation
 //= require govuk_publishing_components/components/table
 
 //= require ./domain-config
