c03f1f1ee3f77e2258ceb24718a9ba1f1d1e73d8
Lucas Mbiwe <lucas.mbiwe@dsit.gov.uk>
2026-09-08 10:27:39 +0100

    Update outdated PACT_CONSUMER_VERSION fallback
    The default identifier of the Pact broker is not longer 'master' but
    'branch-main', this means this test couldn't have worked in a long time.

--- test/service_consumers/pact_helper.rb
+++ test/service_consumers/pact_helper.rb
@@ -38,7 +38,7 @@ Pact.service_provider "Signon API" do
       base_url = ENV.fetch("PACT_BROKER_BASE_URL", "https://govuk-pact-broker-6991351eca05.herokuapp.com")
       url = "#{base_url}/pacts/provider/#{url_encode(name)}/consumer/#{url_encode(consumer_name)}"
 
-      pact_uri "#{url}/versions/#{url_encode(ENV.fetch('PACT_CONSUMER_VERSION', 'master'))}"
+      pact_uri "#{url}/versions/#{url_encode(ENV.fetch('PACT_CONSUMER_VERSION', 'branch-main'))}"
     end
   end
 end
